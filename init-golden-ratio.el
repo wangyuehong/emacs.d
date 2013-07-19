68f424b18eafcf5372078d278cfe4cdefc90a56d
ou.g <ou.g@shanon.co.jp>
2013-07-20 01:11:39 +0900

    disable golden-ratio in ediff

--- init-golden-ratio.el
+++ init-golden-ratio.el
@@ -2,6 +2,5 @@
 
 (require 'golden-ratio)
 (golden-ratio-mode 1)
-(setq golden-ratio-exclude-modes '("speedbar-mode"))
-
+(setq golden-ratio-exclude-modes '("ediff-mode" "speedbar-mode"))
 (provide 'init-golden-ratio)
