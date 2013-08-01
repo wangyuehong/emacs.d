09cce7a6a7f5d2c0ec18ab91f49e541eae5398a9
wangyuehong <wangyuehong@gmail.com>
2013-08-01 23:39:06 +0900

    update ctags for mac and rails

--- init-ctags.el
+++ init-ctags.el
@@ -4,4 +4,7 @@
 (global-set-key (kbd "<f9>") 'ctags-create-or-update-tags-table)
 (setq tags-revert-without-query t)
 
+(when *is-a-mac*
+  (setq ctags-command "/usr/local/bin/ctags -e -R "))
+
 (provide 'init-ctags)
--- init-rails.el
+++ init-rails.el
@@ -8,7 +8,6 @@
 (defun update-rails-ctags ()
   (interactive)
   (let ((default-directory (or (rinari-root) default-directory)))
-    (shell-command (concat "ctags -a -e -f " rinari-tags-file-name " --tag-relative -R app lib vendor test"))))
-
+    (shell-command (concat ctags-command " -a -f " rinari-tags-file-name " --tag-relative -R app lib vendor test"))))
 
 (provide 'init-rails)
