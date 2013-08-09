750e93c7d212e2077929fd9db69fac09bff5a5c8
ou.g <ou.g@shanon.co.jp>
2013-08-09 14:30:14 +0900

    disable golden-ratio mode for ediff

--- init-golden-ratio.el
+++ init-golden-ratio.el
@@ -3,4 +3,13 @@
 (require 'golden-ratio)
 (golden-ratio-mode 1)
 (setq golden-ratio-exclude-modes '("ediff-mode" "speedbar-mode"))
+
+;; disable golden-ratio mode for ediff
+(add-hook 'ediff-before-setup-hook
+          (lambda ()
+            (golden-ratio-mode -1)))
+(add-hook 'ediff-quit-hook
+          (lambda ()
+            (golden-ratio-mode 1)))
+
 (provide 'init-golden-ratio)
