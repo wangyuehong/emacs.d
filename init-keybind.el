7f7b0cb44065d29ce63227dd679fd1f40883547a
ou.g <ou.g@shanon.co.jp>
2013-08-20 15:56:36 +0900

    use vi command

--- init-keybind.el
+++ init-keybind.el
@@ -8,11 +8,11 @@
 (require-package 'goto-last-change)
 (require 'goto-last-change)
 
-(global-set-key (kbd "C-c C-c") 'whole-line-or-region-kill-ring-save)
+;;(global-set-key (kbd "C-c C-c") 'whole-line-or-region-kill-ring-save)
 ;;(global-set-key (kbd "C-c c") 'thing-copy-word)
-(global-set-key (kbd "C-x C-x") 'whole-line-or-region-kill-region)
+;;(global-set-key (kbd "C-x C-x") 'whole-line-or-region-kill-region)
 ;;(global-set-key (kbd "C-x x") 'thing-paste-word)
-(global-set-key (kbd "C-v") 'whole-line-or-region-yank)
+;;(global-set-key (kbd "C-v") 'whole-line-or-region-yank)
 
 ;;get this work for terminal
 (define-key input-decode-map "\e\eOA" [(meta up)])
