f17a8969a3949d7aecc3abf348f396e35cd34b3b
wangyuehong <wangyuehong@gmail.com>
2013-09-14 02:41:40 +0900

    update evil config

--- init-evil.el
+++ init-evil.el
@@ -5,28 +5,26 @@
 (require 'surround)
 (global-surround-mode 1)
 
-(setq evil-default-cursor t) ;; see http://blog.gmane.org/gmane.emacs.vim-emulation/month=20110801
-(setq evil-cross-lines t)
-
 (add-to-list 'evil-emacs-state-modes 'org-mode)
+
+(setq evil-default-cursor t)
+(setq evil-cross-lines t)
+(setq evil-search-module 'evil-search)
+(setq evil-ex-search-vim-style-regexp t)
 
 (setq-default evil-auto-indent t)
 (setq evil-shift-width 4)
-(setq evil-repeat-move-cursor t)
 (setq evil-find-skip-newlines nil)
-(setq evil-move-cursor-back t)
-(setq evil-want-fine-undo nil)
-(setq evil-regexp-search t)
-(setq evil-search-wrap t)
-(setq evil-flash-delay 3)
-(setq evil-want-C-i-jump nil)
-(setq evil-want-C-u-scroll nil)
+(setq evil-move-cursor-back nil)
+(setq evil-want-fine-undo t)
 
 ;;ace-jump
-(define-key evil-normal-state-map (kbd "SPC") 'ace-jump-mode)
+(define-key evil-normal-state-map (kbd "SPC") 'ace-jump-char-mode)
+(define-key evil-visual-state-map (kbd "SPC") 'ace-jump-char-mode)
 
 (define-key evil-insert-state-map (kbd "C-e") 'move-end-of-line)
 (define-key evil-insert-state-map (kbd "C-k") 'kill-line)
+
 (define-key evil-normal-state-map "Y" (kbd "y$"))
 
 (key-chord-define evil-insert-state-map ";;" "\C-e;")
@@ -51,14 +49,17 @@
 
 (setq evil-leader/leader "," evil-leader/in-all-states t)
 (evil-leader/set-key
-  "a"  'ag-project
+  "a"  'ag
+  "A"  'ag-project
+  "b"  'ido-switch-buffer
+  "f"  'ido-find-file
+  "F"  'helm-find-files
   "w"  'save-buffer
-  "/" 'evilnc-comment-or-uncomment-lines
+  "l"  'helm-ls-git-ls
+  "/"  'evilnc-comment-or-uncomment-lines
   "W"  'save-some-buffers
   "k"  'kill-buffer-and-window
   "K"  'kill-other-buffers
-  "p"  'previous-error
-  "n"  'next-error
   "g"  'magit-status
   "r"  'revert-buffer
   "d"  'delete-trailing-whitespace
