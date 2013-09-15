01b80a608e4dfb044890fb143a588ec04226da30
wangyuehong <wangyuehong@gmail.com>
2013-09-15 13:58:27 +0900

    set function scope iedit by default

--- init-iedit.el
+++ init-iedit.el
@@ -1,4 +1,6 @@
 (require-package 'iedit)
-(global-set-key (kbd "C-c e") 'iedit-mode)
+
+(global-set-key (kbd "C-c e") 'iedit-mode-toggle-on-function)
+(global-set-key (kbd "C-c E") 'iedit-mode)
 
 (provide 'init-iedit)
