a9e1a75a46751cec1d70449500cad376b1b27718
wangyuehong <wangyuehong@gmail.com>
2013-09-15 14:32:49 +0900

    add elpa inot recentf exclude config

--- init-recentf.el
+++ init-recentf.el
@@ -1,6 +1,6 @@
 (recentf-mode 1)
 (setq recentf-max-saved-items 1000
-      recentf-exclude '("/tmp/" "/ssh:"))
+      recentf-exclude '("/tmp/" "/ssh:" ".emacs.d/elpa/"))
 
 
 (provide 'init-recentf)
