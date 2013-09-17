a8b7aca4e5ea14b87af686f04fb51575404383b2
wangyuehong <wangyuehong@gmail.com>
2013-09-17 23:41:57 +0900

    add color setting for highlight-symbol

--- init-highlight.el
+++ init-highlight.el
@@ -8,11 +8,13 @@
 (global-set-key (kbd "ESC <f3>") 'highlight-symbol-remove-all)
 (global-set-key (kbd "M-<f3>") 'highlight-symbol-remove-all)
 
+(setq highlight-symbol-colors (quote ("brightgreen" "brightblue" "brightyellow" "brightmagenta" "brightred" "brightcyan")))
+
 (require-package 'hl-line+)
 (require 'hl-line+)
 
 (toggle-hl-line-when-idle 1)
-(hl-line-when-idle-interval 1)
+;; (hl-line-when-idle-interval 3)
 (set-face-background 'hl-line "brightblack")
 
 (provide 'init-highlight)
