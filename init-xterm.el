c89bfeb13a8223813c2cc1b6d74c6c1ce9cc1f4b
ou.g <ou.g@shanon.co.jp>
2013-10-07 13:58:49 +0900

    no mouse support in terminal

--- init-xterm.el
+++ init-xterm.el
@@ -13,12 +13,12 @@
     (define-key map "\e[5C"   [C-right])
     (define-key map "\e[5D"   [C-left])))
 
-(add-hook 'after-make-console-frame-hooks
-          (lambda ()
-            (when (< emacs-major-version 23)
-              (fix-up-xterm-control-arrows))
-            (xterm-mouse-mode 1) ; Mouse in a terminal (Use shift to paste with middle button)
-            (when (fboundp 'mwheel-install)
-              (mwheel-install))))
+;; (add-hook 'after-make-console-frame-hooks
+;;           (lambda ()
+;;             (when (< emacs-major-version 23)
+;;               (fix-up-xterm-control-arrows))
+;;             (xterm-mouse-mode 1) ; Mouse in a terminal (Use shift to paste with middle button)
+;;             (when (fboundp 'mwheel-install)
+;;               (mwheel-install))))
 
 (provide 'init-xterm)
