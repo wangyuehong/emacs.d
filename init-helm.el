004315f78975a2add7a1ba9f0f77e7b5fba97452
wangyuehong <wangyuehong@gmail.com>
2013-10-12 01:43:52 +0900

    do not use helm sometime

--- init-helm.el
+++ init-helm.el
@@ -1,7 +1,19 @@
 (require-package 'helm)
 (require 'helm-config)
 
-(helm-mode 1)
+;; (helm-mode 1)
+
+(setq helm-completing-read-handlers-alist
+      '((describe-function . ido)
+        (describe-variable . ido)
+        (dired-do-copy . nil)
+        (dired-do-rename . nil)
+        (dired-create-directory . nil)
+        (ido-find-file . nil)
+        (ido-edit-input . nil)
+        (read-file-name . ido)
+        (read-directory-name . ido)
+        ))
 
 (global-set-key (kbd "C-x C-b") 'helm-mini)
 (global-set-key (kbd "C-x r h") 'helm-bookmarks)
@@ -9,8 +21,6 @@
 (require-package 'helm-ls-git)
 (require 'helm-ls-git)
 (setq helm-ls-git-show-abs-or-relative 'relative)
-
-(global-set-key (kbd "C-c l") 'helm-ls-git-ls)
 
 (require-package 'helm-git-grep)
 (require 'helm-git-grep)
