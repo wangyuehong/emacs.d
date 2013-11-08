28ca03ba657da8c0bb84f3cf1af9271822873be8
wangyuehong <wangyuehong@gmail.com>
2013-11-09 01:17:41 +0900

    disable whitespace-mode in web-mode

--- init-web-mode.el
+++ init-web-mode.el
@@ -2,6 +2,8 @@
 
 (require 'web-mode)
 
+(add-hook 'web-mode-hook (lambda () (whitespace-mode -1)))
+
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
--- init-whitespace.el
+++ init-whitespace.el
@@ -1,6 +1,8 @@
 (require 'whitespace)
 
-(global-whitespace-mode t)
+;; (global-whitespace-mode t)
+(add-hook 'prog-mode-hook (lambda ()
+                            (whitespace-mode t)))
 (setq whitespace-style
       '(face spaces tabs newline space-mark tab-mark newline-mark))
 (setq whitespace-space-regexp "\\(\x3000+\\)")
