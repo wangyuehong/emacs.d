196ba4dd08e365c6956b3db8891d126fafa25f2f
ou.g <ou.g@shanon.co.jp>
2013-12-15 09:55:18 +0900

    move bookmark+ dotfiles into .emacs.d

--- lisp/init-bookmark.el
+++ lisp/init-bookmark.el
@@ -1,4 +1,7 @@
 (require-package 'bookmark+)
 (require 'bookmark+)
 
+(setq bmkp-bmenu-state-file (expand-file-name ".emacs-bmk-bmenu-state.el" user-emacs-directory))
+(setq bmkp-bmenu-commands-file (expand-file-name ".emacs-bmk-bmenu-commands.el" user-emacs-directory))
+
 (provide 'init-bookmark)
