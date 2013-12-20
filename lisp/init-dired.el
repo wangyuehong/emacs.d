2eaea59f4649b3f5a773ed6ccf9951018522fe6a
Steve Purcell <steve@sanityinc.com>
2013-12-20 21:19:19 +0000

    Don't details in dired by default in trunk emacs
    Conflicts:
    	lisp/init-dired.el

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -1,11 +1,14 @@
 (require-package 'dired+)
 
 (setq diredp-hide-details-initially-flag nil)
-(setq global-dired-hide-details-mode -1)
 (toggle-diredp-find-file-reuse-dir 1)
 
 (after-load 'dired
   (require 'dired+)
+  (when (fboundp 'global-dired-hide-details-mode)
+    (global-dired-hide-details-mode -1))
+  (when (fboundp 'global-dired-hide-details-mode)
+    (global-dired-hide-details-mode -1))
   (setq dired-recursive-deletes 'top)
   (define-key dired-mode-map [mouse-2] 'dired-find-file))
 
