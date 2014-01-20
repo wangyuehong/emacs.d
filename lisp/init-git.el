74171b73c1c77e209a0abf1093c09eea12ef157a
wangyuehong <wangyuehong@gmail.com>
2014-01-20 12:12:08 +0900

    fix helm/magit key-map setting

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -9,8 +9,10 @@
 (require-package 'fullframe)
 (fullframe magit-status magit-mode-quit-window :magit-fullscreen nil)
 
-(define-key magit-status-mode-map (kbd "C-j") 'next-line)
-(define-key magit-status-mode-map (kbd "C-k") 'previous-line)
+(eval-after-load "magit"
+  '(progn
+     (define-key magit-status-mode-map (kbd "C-j") 'next-line)
+     (define-key magit-status-mode-map (kbd "C-k") 'previous-line)))
 
 ;;; When we start working on git-backed files, use git-wip if available
 
