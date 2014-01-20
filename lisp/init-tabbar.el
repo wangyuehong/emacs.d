d27271b528953702aea11dd5c1a618f0397eb8cd
wangyuehong <wangyuehong@gmail.com>
2014-01-20 22:24:56 +0900

    fix tabbar key conflict

--- lisp/init-tabbar.el
+++ lisp/init-tabbar.el
@@ -11,8 +11,12 @@
 ;; conflict with evil-mode
 ;;(global-set-key (kbd "C-c t") 'tabbar-ruler-move)
 
+;; fix keybind conflict in dired-mode
+(define-key dired-mode-map (kbd "M-l") 'tabbar-ruler-forward)
+
 (global-set-key (kbd "M-h") 'tabbar-ruler-backward)
 (global-set-key (kbd "M-l") 'tabbar-ruler-forward)
 (global-set-key (kbd "M-k") 'tabbar-ruler-up)
+(global-set-key (kbd "M-j") 'tabbar-ruler-up)
 
 (provide 'init-tabbar)
