e6b2cf53462d72e02326932c74b14315b87c6fdc
Steve Purcell <steve@sanityinc.com>
2014-05-15 14:24:05 +0100

    Show guide-key hints for % sequences in dired

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -10,7 +10,10 @@
   (when (fboundp 'global-dired-hide-details-mode)
     (global-dired-hide-details-mode -1))
   (setq dired-recursive-deletes 'top)
-  (define-key dired-mode-map [mouse-2] 'dired-find-file))
+  (define-key dired-mode-map [mouse-2] 'dired-find-file)
+  (add-hook 'dired-mode-hook
+            (lambda () (guide-key/add-local-guide-key-sequence "%"))))
+
 
 ;; hide .DS_Store on mac
 (when *is-a-mac*
