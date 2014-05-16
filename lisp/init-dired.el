adc8a30c53bd7c8c9e805663cc652de1314f9e56
ou.g <ou.g@shanon.co.jp>
2014-05-16 12:06:58 +0900

    Revert "Show guide-key hints for % sequences in dired"
    This reverts commit e6b2cf53462d72e02326932c74b14315b87c6fdc.

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -10,10 +10,7 @@
   (when (fboundp 'global-dired-hide-details-mode)
     (global-dired-hide-details-mode -1))
   (setq dired-recursive-deletes 'top)
-  (define-key dired-mode-map [mouse-2] 'dired-find-file)
-  (add-hook 'dired-mode-hook
-            (lambda () (guide-key/add-local-guide-key-sequence "%"))))
-
+  (define-key dired-mode-map [mouse-2] 'dired-find-file))
 
 ;; hide .DS_Store on mac
 (when *is-a-mac*
