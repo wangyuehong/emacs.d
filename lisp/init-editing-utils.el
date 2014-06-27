1a1f43757aa8d4374b3201cdf5fc0d239431c038
ou.g <ou.g@shanon.co.jp>
2014-06-27 10:29:35 +0900

    show current face

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -203,32 +203,14 @@
 
 
 ;;----------------------------------------------------------------------------
-;; Cut/copy the current line if no region is active
+;; show current face name
 ;;----------------------------------------------------------------------------
-;; (whole-line-or-region-mode t)
-;; (diminish 'whole-line-or-region-mode)
-;; (make-variable-buffer-local 'whole-line-or-region-mode)
+(defun current-face (pos)
+  (interactive "d")
+  (let ((face (or (get-char-property (point) 'read-face-name)
+                  (get-char-property (point) 'face))))
+    (if face (message "Face: %s" face) (message "No face at %d" pos))))
 
-;; (defun suspend-mode-during-cua-rect-selection (mode-name)
-;;   "Add an advice to suspend `MODE-NAME' while selecting a CUA rectangle."
-;;   (let ((flagvar (intern (format "%s-was-active-before-cua-rectangle" mode-name)))
-;;         (advice-name (intern (format "suspend-%s" mode-name))))
-;;     (eval-after-load 'cua-rect
-;;       `(progn
-;;          (defvar ,flagvar nil)
-;;          (make-variable-buffer-local ',flagvar)
-;;          (defadvice cua--activate-rectangle (after ,advice-name activate)
-;;            (setq ,flagvar (and (boundp ',mode-name) ,mode-name))
-;;            (when ,flagvar
-;;              (,mode-name 0)))
-;;          (defadvice cua--deactivate-rectangle (after ,advice-name activate)
-;;            (when ,flagvar
-;;              (,mode-name 1)))))))
-
-;; (suspend-mode-during-cua-rect-selection 'whole-line-or-region-mode)
-
-
-
 
 (defun sanityinc/open-line-with-reindent (n)
   "A version of `open-line' which reindents the start and end positions.
