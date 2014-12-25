41e72441e387cd35a0b0b12845f55bda2a25bd5c
wangyuehong <wangyuehong@gmail.com>
2014-12-25 22:55:01 +0900

    replace autocomplete by company

---
+++ lisp/init-company.el
@@ -0,0 +1,17 @@
+(require-package 'company)
+
+(global-company-mode)
+
+(define-key company-active-map (kbd "TAB") 'company-select-next)
+(define-key company-active-map (kbd "C-j") 'company-select-next)
+(define-key company-active-map (kbd "C-k") 'company-select-previous)
+
+(setq
+ company-idle-delay 0.1
+ company-show-numbers t
+ company-selection-wrap-around t
+ company-require-match nil)
+
+(global-set-key (kbd "C-c y") 'company-yasnippet)
+
+(provide 'init-company)
