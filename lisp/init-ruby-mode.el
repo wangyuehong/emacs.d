385ef624c542316a4dcaaaa1a00bd51a7130dca2
ou.g <ou.g@shanon.co.jp>
2015-02-13 11:21:40 +0900

    update ruby-mode

--- lisp/init-ruby-mode.el
+++ lisp/init-ruby-mode.el
@@ -10,30 +10,57 @@
 (setq ruby-use-encoding-map nil)
 
 (after-load 'ruby-mode
-  (setq-default ruby-use-smie nil)
-  (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
-  ;; (define-key ruby-mode-map (kbd "TAB") 'indent-for-tab-command)
-  )
+  (define-key ruby-mode-map (kbd "TAB") 'indent-for-tab-command)
 
-(defun my/ruby-mode-hook ()
   ;; Stupidly the non-bundled ruby-mode isn't a derived mode of
   ;; prog-mode: we run the latter's hooks anyway in that case.
-  (unless (derived-mode-p 'prog-mode)
-    (run-hooks 'prog-mode-hook)))
+  (add-hook 'ruby-mode-hook
+            (lambda ()
+              (unless (derived-mode-p 'prog-mode)
+                (run-hooks 'prog-mode-hook)))))
 
-(add-hook 'ruby-mode-hook 'my/ruby-mode-hook)
+(add-hook 'ruby-mode-hook 'subword-mode)
+
+;; TODO: hippie-expand ignoring : for names in ruby-mode
+;; TODO: hippie-expand adaptor for auto-complete sources
 
 ;;; Inferior ruby
 (require-package 'inf-ruby)
+(require-package 'ac-inf-ruby)
+(after-load 'auto-complete
+  (add-to-list 'ac-modes 'inf-ruby-mode))
+(add-hook 'inf-ruby-mode-hook 'ac-inf-ruby-enable)
+(after-load 'inf-ruby
+  (define-key inf-ruby-mode-map (kbd "TAB") 'auto-complete))
+
 
 ;;; Ruby compilation
 (require-package 'ruby-compilation)
+
+(after-load 'ruby-mode
+  (let ((m ruby-mode-map))
+    (define-key m [S-f7] 'ruby-compilation-this-buffer)
+    (define-key m [f7] 'ruby-compilation-this-test)
+    (define-key m [f6] 'recompile)))
+
+;;; Robe
+(require-package 'robe)
+(after-load 'ruby-mode
+  (add-hook 'ruby-mode-hook 'robe-mode))
+(after-load 'robe
+  (add-hook 'robe-mode-hook
+            (lambda ()
+              (add-to-list 'ac-sources 'ac-source-robe)
+              (set-auto-complete-as-completion-at-point-function))))
+
 
 ;;; ri support
 (require-package 'yari)
 (defalias 'ri 'yari)
 
+
 ;;; YAML
+
 (require-package 'yaml-mode)
 
 ;;; rails config
@@ -90,12 +117,6 @@
     (define-key wangyh/rinari-minor-mode-find-map "y" 'rinari-find-stylesheet)
     (define-key wangyh/rinari-minor-mode-find-map "z" 'rinari-find-rspec-fixture)
     ))
-
-(require-package 'haml-mode)
-(require 'haml-mode)
-
-(require-package 'slim-mode)
-(require 'slim-mode)
 
 (defun update-rails-ctags ()
   (interactive)
