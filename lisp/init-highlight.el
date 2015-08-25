94fc63e783619f3cc17263e9b5e703daff0a279d
wangyuehong <wangyuehong@gmail.com>
2015-08-25 22:15:38 +0900

    update highlight-symbol

--- lisp/init-highlight.el
+++ lisp/init-highlight.el
@@ -4,11 +4,11 @@
 
 (global-set-key (kbd "<f3>") 'highlight-symbol-next)
 (global-set-key (kbd "<f4>") 'highlight-symbol-prev)
-(global-set-key (kbd "<f5>") 'highlight-symbol-at-point)
+(global-set-key (kbd "<f5>") 'highlight-symbol)
 (global-set-key (kbd "ESC <f3>") 'highlight-symbol-remove-all)
 (global-set-key (kbd "M-<f3>") 'highlight-symbol-remove-all)
 
-(setq highlight-symbol-colors (quote ("#5c5cff" "#ff0000" "#00ff00" "#ff00ff" "#ffff00" "#00ffff")))
+(setq highlight-symbol-colors (quote ("#5c5cff" "#ff0000" "#00ff00" "#ff00ff" "#ffff00")))
 
 ;; (add-hook 'prog-mode-hook 'hl-line-mode)
 
