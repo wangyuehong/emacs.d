c042051730fd0c550a69b58afb3cb0eff22f090c
wangyuehong <wangyuehong@gmail.com>
2015-08-31 00:50:29 +0900

    bye hlinum

--- lisp/init-linum.el
+++ lisp/init-linum.el
@@ -1,11 +1,11 @@
 (global-linum-mode 1)
 (setq linum-format 'dynamic)
 
-(require-package 'hlinum)
-(require 'hlinum)
-(hlinum-activate)
+;; (require-package 'hlinum)
+;; (require 'hlinum)
+;; (hlinum-activate)
 
-(set-face-foreground 'linum-highlight-face "#ffffff")
-(set-face-background 'linum-highlight-face "#ff0000")
+;;(set-face-foreground 'linum-highlight-face "#ffffff")
+;;(set-face-background 'linum-highlight-face "#ff0000")
 
 (provide 'init-linum)
