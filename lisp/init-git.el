8d02df2ae2d292ebe8c205c08c3d8ad4e86caeb2
wangyuehong <wangyuehong@gmail.com>
2015-08-31 00:51:29 +0900

    git-gutter works with linum

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -113,7 +113,6 @@
 
 (setq git-gutter:update-threshold 2)
 (setq git-gutter:update-hooks '(after-save-hook after-revert-hook))
-;; (setq git-gutter:diff-option "HEAD")
 
 (define-prefix-command 'git-gutter-map)
 (global-set-key (kbd "C-x g") 'git-gutter-map)
@@ -126,51 +125,15 @@
   ;; terminal
   (progn
     (require 'git-gutter)
-    ;; (global-git-gutter-mode t)
-    (setq git-gutter:separator-sign "|")
-
-    (define-key git-gutter-map "t" 'toggle-git-gutter-linum)))
-
-(defun toggle-git-gutter-linum ()
-  "toggle git-gutter and linum mode."
-  (interactive)
-  (if (or linum-mode git-gutter-mode)
-      (if linum-mode
-          (progn
-              (linum-mode 0)
-              (git-gutter-mode 1))
-          (progn
-              (git-gutter-mode 0)
-              (linum-mode 1)))))
-
-(defun change-git-gutter-diff-option (option)
-  "set git-gutter:diff-option and reload"
-  (setq git-gutter:diff-option option)
-  (git-gutter))
-
-(defun git-gutter-diff-none ()
-  "git-gutter with git diff, no diff if staged"
-  (interactive)
-  (change-git-gutter-diff-option ""))
-
-(defun git-gutter-diff-head ()
-  "git-gutter with git HEAD, include diff of staged and unstaged"
-  (interactive)
-  (change-git-gutter-diff-option "HEAD"))
-
-(defun git-gutter-diff-cached ()
-  "git-gutter with git --cached, only show diff of staged"
-  (interactive)
-  (change-git-gutter-diff-option "--cached"))
+    (global-git-gutter-mode t)
+    (git-gutter:linum-setup)
+    )
+  )
 
 (define-key git-gutter-map "j" 'git-gutter:next-hunk)
 (define-key git-gutter-map "k" 'git-gutter:previous-hunk)
 (define-key git-gutter-map "p" 'git-gutter:popup-hunk)
 (define-key git-gutter-map "v" 'git-gutter:revert-hunk)
-(define-key git-gutter-map "dn" 'git-gutter-diff-none)
-(define-key git-gutter-map "dh" 'git-gutter-diff-head)
-(define-key git-gutter-map "dc" 'git-gutter-diff-cached)
-(define-key git-gutter-map "l" 'magit-file-log)
 
 (when *is-a-mac*
   (after-load 'magit
