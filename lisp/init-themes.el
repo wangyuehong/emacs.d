33c31e04d6acf0a31e00a9aad885dfa0612b703c
ou.g <ou.g@shanon.co.jp>
2016-03-01 21:29:18 +0900

    delete useless themes

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,7 +1,4 @@
 (require-package 'color-theme-sanityinc-solarized)
 (require-package 'color-theme-sanityinc-tomorrow)
-(require-package 'ample-theme)
-(require-package 'moe-theme)
-(require 'moe-theme)
 
 (provide 'init-themes)
