35e4038819fe2d4ec514b3a484a3d7b981619cfa
ou.g <ou.g@shanon.co.jp>
2016-04-17 17:03:51 +0900

    web-mode indent

--- lisp/init-web-mode.el
+++ lisp/init-web-mode.el
@@ -18,6 +18,10 @@
 (setq web-mode-enable-comment-keywords t)
 (setq web-mode-enable-current-element-highlight nil)
 
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
+
 (set-face-foreground 'web-mode-html-tag-face "#268bd2")
 (set-face-foreground 'web-mode-html-attr-name-face "#dc322f")
 (set-face-foreground 'web-mode-html-attr-equal-face "#268bd2")
