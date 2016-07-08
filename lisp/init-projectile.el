71bd5f276ee6a9ac5d9123515bd5ec45dd2da19e
ou.g <ou.g@shanon.co.jp>
2016-07-08 11:31:05 +0900

    update projectile config

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -1,11 +1,21 @@
-(require-package 'projectile)
-(projectile-global-mode)
+(when (maybe-require-package 'projectile)
+  (add-hook 'after-init-hook 'projectile-global-mode)
+  )
 
-(setq projectile-completion-system 'helm)
-(setq projectile-enable-caching t)
-(setq projectile-switch-project-action 'projectile-dired)
+(after-load 'projectile
+  (setq projectile-completion-system 'helm)
+  (setq projectile-enable-caching t)
+  (setq projectile-switch-project-action 'projectile-dired)
 
-(require-package 'helm-projectile)
-(helm-projectile-on)
+  (require-package 'helm-projectile)
+  (helm-projectile-on)
+
+  (setq-default
+   projectile-mode-line
+   '(:eval
+     (if (file-remote-p default-directory)
+         " Pr"
+       (format " Pr[%s]" (projectile-project-name)))))
+  )
 
 (provide 'init-projectile)
