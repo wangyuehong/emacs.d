748c698b5def0c2ca00cdde346772b14d5d560b1
Steve Purcell <steve@sanityinc.com>
2016-07-17 14:07:32 +1200

    Prefer setq-default to setq for recentf config

--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -1,6 +1,7 @@
 (recentf-mode 1)
-(setq recentf-max-saved-items 1000
-      recentf-exclude '("/tmp/" "/ssh:" ".emacs.d/elpa/"))
+(setq-default
+ recentf-max-saved-items 1000
+ recentf-exclude '("/tmp/" "/ssh:"))
 
 (setq recentf-save-file (expand-file-name ".recentf" user-emacs-directory))
 
