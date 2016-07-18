b6c029d18b8f08d5b9491c8294de82a75cc7d1aa
Steve Purcell <steve@sanityinc.com>
2016-07-18 13:06:24 +1200

    Remove non-working OSX color emoji support code
    http://emacs.1067599.n5.nabble.com/Emoji-on-Mac-OS-X-td401600.html

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -1,49 +1,24 @@
-(require 'cl)
+;;; Character sets
 
 
-(defun sanityinc/font-name-replace-size (font-name new-size)
-  (let ((parts (split-string font-name "-")))
-    (setcar (nthcdr 7 parts) (format "%d" new-size))
-    (mapconcat 'identity parts "-")))
+;;; Changing font sizes
 
-(defun sanityinc/increment-default-font-height (delta)
-  "Adjust the default font height by DELTA on every frame.
-Emacs will keep the pixel size of the frame approximately the
-same.  DELTA should be a multiple of 10, to match the units used
-by the :height face attribute."
-  (let* ((new-height (+ (face-attribute 'default :height) delta))
-         (new-point-height (/ new-height 10)))
-    (dolist (f (frame-list))
-      (with-selected-frame f
-        ;; Latest 'set-frame-font supports a "frames" arg, but
-        ;; we cater to Emacs 23 by looping instead.
-        (set-frame-font (sanityinc/font-name-replace-size
-                         (face-font 'default)
-                         new-point-height)
-                        t)))
-    (set-face-attribute 'default nil :height new-height)
-    (message "default font size is now %d" new-point-height)))
+(require-package 'default-text-scale)
+(global-set-key (kbd "C-M-=") 'default-text-scale-increase)
+(global-set-key (kbd "C-M--") 'default-text-scale-decrease)
 
-(defun sanityinc/increase-default-font-height ()
-  (interactive)
-  (sanityinc/increment-default-font-height 10))
 
-(defun sanityinc/decrease-default-font-height ()
-  (interactive)
-  (sanityinc/increment-default-font-height -10))
+(defun sanityinc/maybe-adjust-visual-fill-column ()
+  "Readjust visual fill column when the global font size is modified.
+This is helpful for writeroom-mode, in particular."
+  ;; TODO: submit as patch
+  (if visual-fill-column-mode
+      (add-hook 'after-setting-font-hook 'visual-fill-column--adjust-window nil t)
+    (remove-hook 'after-setting-font-hook 'visual-fill-column--adjust-window t)))
 
-(global-set-key (kbd "C-M-=") 'sanityinc/increase-default-font-height)
-(global-set-key (kbd "C-M--") 'sanityinc/decrease-default-font-height)
+(add-hook 'visual-fill-column-mode-hook
+          'sanityinc/maybe-adjust-visual-fill-column)
 
-(when (window-system)
-  ;; English Font
-  ;; (set-face-attribute 'default nil :font "Menlo 12")
 
-  ;; CJK Font
-  ;; (dolist (charset '(kana han symbol cjk-misc bopomofo))
-  ;;   (set-fontset-font (frame-parameter nil 'font)
-  ;;                     charset (font-spec :family "Microsoft Yahei"
-  ;;                                        :size 12)))
-  )
 
 (provide 'init-fonts)
