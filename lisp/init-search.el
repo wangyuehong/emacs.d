8b26330f163212d6923833eadaacaa0d7bb6eb07
Steve Purcell <steve@sanityinc.com>
2016-08-15 12:08:24 +1200

    Elide anzu's mode lighter using its own custom var

--- lisp/init-search.el
+++ lisp/init-search.el
@@ -1,10 +1,9 @@
 ;; Show number of matches while searching
-(require-package 'anzu)
-(global-anzu-mode t)
-(diminish 'anzu-mode)
-(global-set-key [remap query-replace-regexp] 'anzu-query-replace-regexp)
-(global-set-key [remap query-replace] 'anzu-query-replace)
-
+(when (maybe-require-package 'anzu)
+  (global-anzu-mode t)
+  (setq anzu-mode-lighter "")
+  (global-set-key [remap query-replace-regexp] 'anzu-query-replace-regexp)
+  (global-set-key [remap query-replace] 'anzu-query-replace))
 
 ;; Activate occur easily inside isearch
 (define-key isearch-mode-map (kbd "C-o") 'isearch-occur)
