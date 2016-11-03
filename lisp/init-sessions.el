50149c5f5a426012af2c85ab26ccdc5a791d5f4a
Steve Purcell <steve@sanityinc.com>
2016-11-03 21:09:43 +1300

    Remove frame-restore support in older emacs versions
    See https://github.com/melpa/melpa/pull/4380 for discussion.

--- lisp/init-sessions.el
+++ lisp/init-sessions.el
@@ -25,34 +25,48 @@
 ;;----------------------------------------------------------------------------
 ;; Restore histories and registers after saving
 ;;----------------------------------------------------------------------------
+(setq-default history-length 1000)
+(savehist-mode t)
+
 (require-package 'session)
+
+(setq session-save-file (expand-file-name ".session" user-emacs-directory))
+(setq session-name-disable-regexp "\\(?:\\`'/tmp\\|\\.git/[A-Z_]+\\'\\)")
+(add-hook 'after-init-hook 'session-initialize)
 
 ;; make it work with helm-show-kill-ring
 (setq session-save-print-spec '(t nil 40000))
 
-(setq session-save-file (expand-file-name ".session" user-emacs-directory))
-(add-hook 'after-init-hook 'session-initialize)
-
 ;; save a bunch of variables to the desktop file
 ;; for lists specify the len of the maximal saved data also
 (setq desktop-globals-to-save
-      (append '((extended-command-history . 30)
+      (append '((comint-input-ring        . 50)
+                (compile-history          . 30)
+                desktop-missing-file-warning
+                (dired-regexp-history     . 20)
+                (extended-command-history . 30)
+                (face-name-history        . 20)
                 (file-name-history        . 100)
+                (grep-find-history        . 30)
+                (grep-history             . 30)
+                (ido-buffer-history       . 100)
                 (ido-last-directory-list  . 100)
                 (ido-work-directory-list  . 100)
                 (ido-work-file-list       . 100)
-                (grep-history             . 30)
-                (compile-history          . 30)
+                (ivy-history              . 100)
+                (magit-read-rev-history   . 50)
                 (minibuffer-history       . 50)
+                (org-clock-history        . 50)
+                (org-refile-history       . 50)
+                (org-tags-history         . 50)
                 (query-replace-history    . 60)
                 (read-expression-history  . 60)
                 (regexp-history           . 60)
                 (regexp-search-ring       . 20)
+                register-alist
                 (search-ring              . 20)
-                (comint-input-ring        . 50)
                 (shell-command-history    . 50)
-                desktop-missing-file-warning
                 tags-file-name
-                register-alist)))
+                tags-table-list)))
 
 (provide 'init-sessions)
