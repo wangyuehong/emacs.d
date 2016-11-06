3aa5a792a10ff934856d76631f3134f698ab3e21
Steve Purcell <steve@sanityinc.com>
2016-11-06 16:31:03 +1300

    Remove unused helper functions

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -29,20 +29,6 @@
       (setq pos (match-end group)))
     result))
 
-(defun string-rtrim (str)
-  "Remove trailing whitespace from `STR'."
-  (replace-regexp-in-string "[ \t\n]*$" "" str))
-
-
-;;----------------------------------------------------------------------------
-;; Find the directory containing a given library
-;;----------------------------------------------------------------------------
-(autoload 'find-library-name "find-func")
-(defun directory-of-library (library-name)
-  "Return the directory in which the `LIBRARY-NAME' load file is found."
-  (file-name-as-directory (file-name-directory (find-library-name library-name))))
-
-
 ;;----------------------------------------------------------------------------
 ;; Delete the current file
 ;;----------------------------------------------------------------------------
