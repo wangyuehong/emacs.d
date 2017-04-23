40bdf071749c4de1d7804aa55d39a85d7796855f
wangyuehong <wangyuehong@gmail.com>
2017-04-23 15:51:31 +0900

    no xterm-mouse

--- lisp/init-xterm.el
+++ lisp/init-xterm.el
@@ -6,7 +6,7 @@
 (autoload 'mwheel-install "mwheel")
 
 (defun sanityinc/console-frame-setup ()
-  (xterm-mouse-mode 1) ; Mouse in a terminal (Use shift to paste with middle button)
+  ;; (xterm-mouse-mode 1) ; Mouse in a terminal (Use shift to paste with middle button)
   (mwheel-install))
 
 
