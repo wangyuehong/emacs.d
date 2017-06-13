149948ef1807258e6b437997c0c6f022f8cfc18d
ou.g <ou.g@shanon.co.jp>
2017-06-13 23:19:38 +0900

    set .ep using web-mode

--- lisp/init-web-mode.el
+++ lisp/init-web-mode.el
@@ -18,6 +18,7 @@
 (add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ep\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.ftl\\'" . web-mode))
