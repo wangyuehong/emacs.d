7b3c0b6ec6fa4effd0a7a88de32a85123423d6e4
Steve Purcell <steve@sanityinc.com>
2017-08-09 19:00:07 +1200

    Drop rinari for Rails work: projectile-rails is sufficient

--- lisp/init-rails.el
+++ lisp/init-rails.el
@@ -1,17 +1,7 @@
-(when (maybe-require-package 'rinari)
-  (after-load 'rinari
-    (diminish 'rinari-minor-mode "Rin"))
-  (global-rinari-mode))
-
-;; (defun update-rails-ctags ()
-;;   (interactive)
-;;   (let ((default-directory (or (rinari-root) default-directory)))
-;;     (shell-command (concat "ctags -a -e -f " rinari-tags-file-name " --tag-relative -R app lib vendor test"))))
-
-(defun update-rails-ctags ()
-  (interactive)
-  (let ((default-directory (or (rinari-root) default-directory)))
-    ;; use universal-ctags
-    (shell-command "rails runner 'system(%q(ctags --language-force=ruby -e -R ) + $:.map(&:to_s).select {|d|Dir.exists? d}.uniq.join(%q( )))'")))
+(when (maybe-require-package 'projectile-rails)
+  (after-load 'projectile
+    (add-hook 'projectile-mode-hook 'projectile-rails-on)
+    (after-load 'guide-key
+      (add-to-list 'guide-key/guide-key-sequence "C-c r"))))
 
 (provide 'init-rails)
