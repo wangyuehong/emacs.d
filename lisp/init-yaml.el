cf8926b0f3c3eed4adba9182e7c61bce11ff9dee
wangyuehong <wangyuehong@gmail.com>
2017-09-10 16:42:42 +0900

    update init-yaml-mode

--- lisp/init-yaml.el
+++ lisp/init-yaml.el
@@ -1,3 +1,5 @@
-(require-package 'yaml-mode)
+(when (maybe-require-package 'yaml-mode)
+  (add-auto-mode 'yaml-mode "\\.yml\\.erb\\'")
+  (add-hook 'yaml-mode-hook 'goto-address-prog-mode))
 
 (provide 'init-yaml)
