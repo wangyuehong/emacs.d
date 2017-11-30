8e928ddb083afc085fdb4729613e94a7e3bad667
ou.g <ou.g@shanon.co.jp>
2017-11-30 14:41:26 +0900

    decrease recentf-max-saved-items to 100

--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -1,6 +1,6 @@
 (add-hook 'after-init-hook 'recentf-mode)
 (setq-default
- recentf-max-saved-items 1000
+ recentf-max-saved-items 100
  recentf-exclude '("/tmp/" "/ssh:"))
 
 (setq recentf-save-file (expand-file-name ".recentf" user-emacs-directory))
