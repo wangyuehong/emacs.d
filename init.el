43e30e526e725f17665fc182f1cb8b9904625cff
Steve Purcell <steve@sanityinc.com>
2018-01-27 15:04:56 +1300

    Place loading of init-local after init-locales, to allow overrides
    See #320

--- init.el
+++ init.el
@@ -151,16 +151,15 @@
 
 
 ;;----------------------------------------------------------------------------
-;; Allow users to provide an optional "init-local" containing personal settings
-;;----------------------------------------------------------------------------
-(require 'init-local nil t)
-
-
-;;----------------------------------------------------------------------------
 ;; Locales (setting them earlier in this file doesn't work in X)
 ;;----------------------------------------------------------------------------
 (require 'init-locales)
 
+
+;;----------------------------------------------------------------------------
+;; Allow users to provide an optional "init-local" containing personal settings
+;;----------------------------------------------------------------------------
+(require 'init-local nil t)
 
 
 
