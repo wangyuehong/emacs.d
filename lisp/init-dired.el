d40a3816473de8b4ce7051b0089ea21dd98d2c6b
wangyuehong <wangyuehong@gmail.com>
2018-02-02 10:44:26 +0900

    drop dired-sort

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -1,5 +1,4 @@
 (require-package 'dired+)
-(require-package 'dired-sort)
 
 (toggle-diredp-find-file-reuse-dir 1)
 (setq-default diredp-hide-details-initially-flag nil
@@ -9,7 +8,6 @@
 
 (after-load 'dired
   (require 'dired+)
-  (require 'dired-sort)
   (diredfl-global-mode)
   (when (fboundp 'global-dired-hide-details-mode)
     (global-dired-hide-details-mode -1))
