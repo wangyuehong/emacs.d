620c7d87cef7596ea822492840b338b9f18d0c83
wangyuehong <wangyuehong@gmail.com>
2018-02-03 22:02:28 +0900

    add yasnippet into company-backends

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -54,4 +54,19 @@
     (add-hook 'company-completion-finished-hook 'sanityinc/page-break-lines-maybe-reenable)
     (add-hook 'company-completion-cancelled-hook 'sanityinc/page-break-lines-maybe-reenable)))
 
+(after-load 'company
+  (after-load 'yasnippet
+    ;; Add yasnippet support for all company backends
+    ;; https://github.com/syl20bnr/spacemacs/pull/179
+    (defvar company-mode/enable-yas t
+      "Enable yasnippet for all backends.")
+
+    (defun company-mode/backend-with-yas (backend)
+      (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
+          backend
+        (append (if (consp backend) backend (list backend))
+                '(:with company-yasnippet))))
+
+    (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))))
+
 (provide 'init-company)
