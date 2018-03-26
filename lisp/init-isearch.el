1465f194a218ecc2425f3b75058b6e5cdfcfac60
Steve Purcell <steve@sanityinc.com>
2018-03-26 22:32:16 +1300

    Defer loading of some isearch config

--- lisp/init-isearch.el
+++ lisp/init-isearch.el
@@ -6,9 +6,12 @@
   (global-set-key [remap query-replace] 'anzu-query-replace))
 
 ;; Activate occur easily inside isearch
-(define-key isearch-mode-map (kbd "C-o") 'isearch-occur)
+(after-load 'isearch
+  ;; DEL during isearch should edit the search string, not jump back to the previous result
+  (define-key isearch-mode-map [remap isearch-delete-char] 'isearch-del-char)
 
-;; DEL during isearch should edit the search string, not jump back to the previous result
-(define-key isearch-mode-map [remap isearch-delete-char] 'isearch-del-char)
+  (when (fboundp 'isearch-occur)
+    ;; to match ivy conventions
+    (define-key isearch-mode-map (kbd "C-c C-o") 'isearch-occur)))
 
 (provide 'init-isearch)
