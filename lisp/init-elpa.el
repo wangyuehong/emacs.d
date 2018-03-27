090600b338453a1b30d45b1121af97733540bb15
wangyuehong <wangyuehong@gmail.com>
2018-03-27 23:08:34 +0900

    package-autoremove after auto-package-update

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -96,6 +96,6 @@ locate PACKAGE."
   (auto-package-update-maybe)
   (setq auto-package-update-delete-old-versions t)
   (add-hook 'auto-package-update-before-hook
-            (lambda () (message "update packages now"))))
+            (lambda () (package-autoremove))))
 
 (provide 'init-elpa)
