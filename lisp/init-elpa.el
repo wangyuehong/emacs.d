7360eda3dd1f8c24af9e1f31fcf29f80c4a7b19b
wangyuehong <wangyuehong@gmail.com>
2018-04-02 10:43:39 +0900

    Revert "package-autoremove after auto-package-update"
    This reverts commit 090600b338453a1b30d45b1121af97733540bb15.

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -96,6 +96,6 @@ locate PACKAGE."
   (auto-package-update-maybe)
   (setq auto-package-update-delete-old-versions t)
   (add-hook 'auto-package-update-before-hook
-            (lambda () (package-autoremove))))
+            (lambda () (message "update packages now"))))
 
 (provide 'init-elpa)
