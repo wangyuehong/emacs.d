de47a10a0e7437ee8e6c5798e14f24f1b1509a15
wangyuehong <wangyuehong@gmail.com>
2018-12-28 22:38:01 +0900

    change whitespace background face

--- lisp/init-whitespace.el
+++ lisp/init-whitespace.el
@@ -36,18 +36,23 @@
 (setq whitespace-display-mappings
       '(
         (space-mark ?\x3000 [9633])
-        ;;(space-mark 32 [183] [46]) ; normal space,
+        ;;(space-mark 32 [183] [46]) ; normal space
         (space-mark 160 [164] [95])
         (space-mark 2208 [2212] [95])
         (space-mark 2336 [2340] [95])
         (space-mark 3616 [3620] [95])
         (space-mark 3872 [3876] [95])
-        ;;(newline-mark 10 [8629 10]) ; newlne, ?
-        (tab-mark 9 [187 9] [92 9]) ; tab, ?
+        ;;(newline-mark 10 [8629 10]) ;; newlne
+        (tab-mark 9 [187 9] [92 9]) ;; tab
         ))
 ;; color
-(set-face-foreground 'whitespace-newline "#5c5cff")
+;; (set-face-foreground 'whitespace-newline "#5c5cff")
+;; (set-face-background 'whitespace-newline nil)
+
 (set-face-foreground 'whitespace-space "#5c5cff")
+(set-face-background 'whitespace-space nil)
+
 (set-face-foreground 'whitespace-tab "#5c5cff")
+(set-face-background 'whitespace-tab nil)
 
 (provide 'init-whitespace)
