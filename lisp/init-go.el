47d5cb6a37d028352e1f9f9c60cb7d1346ce53cd
wangyuehong <wangyuehong@gmail.com>
2018-12-28 14:14:45 +0900

    add go-snippets

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -2,6 +2,12 @@
 (require-package 'go-snippets)
 (require-package 'toml-mode)
 (require-package 'golint)
+;; (require-package 'company-go)
+
+;; (add-hook 'go-mode-hook
+;;           (lambda ()
+;;             (set (make-local-variable 'company-backends) '(company-go))
+;;             (company-mode)))
 
 (after-load 'go-mode
   (define-key go-mode-map (kbd "C-c d") 'godoc-at-point)
