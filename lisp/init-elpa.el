ac4adc7113b735f6c8ba4fe4a2e7d1f52f94f044
wangyuehong <wangyuehong@gmail.com>
2019-01-13 14:52:40 +0900

    simplify init-elpa

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -4,7 +4,6 @@
 
 (require 'package)
 
-
 ;;; Install into separate package dirs for each Emacs version, to prevent bytecode incompatibility
 (let ((versioned-package-dir
        (expand-file-name (format "elpa-%s.%s" emacs-major-version emacs-minor-version)
@@ -12,23 +11,13 @@
   (setq package-user-dir versioned-package-dir))
 
 
-
 ;;; Standard package repositories
-
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
        (proto (if no-ssl "http" "https")))
   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;; Official MELPA Mirror, in case necessary.
-  ;;(add-to-list 'package-archives (cons "melpa-mirror" (concat proto "://www.mirrorservice.org/sites/melpa.org/packages/")) t)
-  (if (< emacs-major-version 24)
-      ;; For important compatibility libraries like cl-lib
-      (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))
-    (unless no-ssl
-      ;; Force SSL for GNU ELPA
-      (setcdr (assoc "gnu" package-archives) "https://elpa.gnu.org/packages/"))))
+  )
 
-
 ;;; On-demand installation of packages
 
 (require 'cl-lib)
@@ -59,13 +48,11 @@ locate PACKAGE."
      (message "Couldn't install optional package `%s': %S" package err)
      nil)))
 
-
 ;;; Fire up package.el
 
 (setq package-enable-at-startup nil)
 (package-initialize)
 
-
 ;; package.el updates the saved version of package-selected-packages correctly only
 ;; after custom-file has been loaded, which is a bug. We work around this by adding
 ;; the required packages to package-selected-packages after startup is complete.
@@ -87,11 +74,9 @@ locate PACKAGE."
             (lambda () (package--save-selected-packages
                    (seq-uniq (append sanityinc/required-packages package-selected-packages))))))
 
-
 (require-package 'fullframe)
 (fullframe list-packages quit-window)
 
-
 (defun sanityinc/set-tabulated-list-column-width (col-name width)
   "Set any column with name COL-NAME to the given WIDTH."
   (when (> width (length col-name))
@@ -107,7 +92,6 @@ locate PACKAGE."
       (sanityinc/set-tabulated-list-column-width "Archive" longest-archive-name))))
 
 (add-hook 'package-menu-mode-hook 'sanityinc/maybe-widen-package-menu-columns)
-
 
 (provide 'init-elpa)
 ;;; init-elpa.el ends here
