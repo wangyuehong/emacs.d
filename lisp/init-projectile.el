99daa41eb020f3fad428acf1db34b752c76c7690
wangyuehong <wangyuehong@gmail.com>
2019-01-14 22:32:36 +0900

    update projectile

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -8,9 +8,11 @@
               ("C-c p" . projectile-command-map))
   :hook (after-init . projectile-mode)
   :init
-  (setq projectile-mode-line-prefix "")
+  (setq projectile-mode-line-prefix "P ")
+  (setq projectile-switch-project-action #'projectile-dired)
   (setq projectile-sort-order 'recentf)
   (setq projectile-use-git-grep t)
+  (setq projectile-git-submodule-command nil)
   :config
   (projectile-update-mode-line) ; Update mode-line at the first time
   )
