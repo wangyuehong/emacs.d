68c77c68a1cc78637bcd3bea74bd0351e38d8615
wangyuehong <wangyuehong@gmail.com>
2019-01-15 22:04:59 +0900

    update init-isearch

--- lisp/init-isearch.el
+++ lisp/init-isearch.el
@@ -1,9 +1,17 @@
+;;; init-isearch.el --- isearch settings -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
 ;; Show number of matches while searching
-(when (maybe-require-package 'anzu)
-  (add-hook 'after-init-hook 'global-anzu-mode)
+(use-package anzu
+  :init
   (setq anzu-mode-lighter "")
+  :hook
+  (after-init . global-anzu-mode)
+  :config
   (global-set-key [remap query-replace-regexp] 'anzu-query-replace-regexp)
-  (global-set-key [remap query-replace] 'anzu-query-replace))
+  (global-set-key [remap query-replace] 'anzu-query-replace)
+  )
 
 ;; Activate occur easily inside isearch
 (after-load 'isearch
@@ -15,3 +23,4 @@
     (define-key isearch-mode-map (kbd "C-c C-o") 'isearch-occur)))
 
 (provide 'init-isearch)
+;;; init-isearch.el ends here
