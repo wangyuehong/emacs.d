8ff0d43c3d53a658cf553e54614c4501dd066ad8
wangyuehong <wangyuehong@gmail.com>
2019-01-15 22:58:00 +0900

    customize recentf file path

--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -4,6 +4,7 @@
 
 (add-hook 'after-init-hook 'recentf-mode)
 (setq-default
+ recentf-save-file (expand-file-name ".recentf" user-emacs-directory)
  recentf-max-saved-items 1000
  recentf-exclude '("/tmp/" "/ssh:"))
 
