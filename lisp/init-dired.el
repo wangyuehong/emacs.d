e8abad3f832fa6f8ee6c3f8440d8b3740061facd
wangyuehong <wangyuehong@gmail.com>
2019-01-15 11:18:57 +0900

    show dired in a normal way

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -14,18 +14,6 @@
   :config
   (diredfl-global-mode))
 
-(defun +dired|sort-directories-first ()
-  "List directories first in dired buffers."
-  (save-excursion
-    (let (buffer-read-only)
-      (forward-line 2) ;; beyond dir. header
-      (sort-regexp-fields t "^.*$" "[ ]*." (point) (point-max))))
-  (and (featurep 'xemacs)
-       (fboundp 'dired-insert-set-properties)
-       (dired-insert-set-properties (point-min) (point-max)))
-  (set-buffer-modified-p nil))
-(add-hook 'dired-after-readin-hook #'+dired|sort-directories-first)
-
 ;; Automatically create missing directories when creating new files
 (defun +dired|create-non-existent-directory ()
   (let ((parent-directory (file-name-directory buffer-file-name)))
