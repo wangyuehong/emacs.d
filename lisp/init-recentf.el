85a18b0921f6d526989893b1227510cb50ad6ee3
wangyuehong <wangyuehong@gmail.com>
2019-01-15 22:36:17 +0900

    use-package for ibuffer ivy recentf uniquify

--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -1,8 +1,11 @@
+;;; init-recentf.el --- Settings for tracking recent files -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
 (add-hook 'after-init-hook 'recentf-mode)
 (setq-default
- recentf-max-saved-items 100
+ recentf-max-saved-items 1000
  recentf-exclude '("/tmp/" "/ssh:"))
 
-(setq recentf-save-file (expand-file-name ".recentf" user-emacs-directory))
-
 (provide 'init-recentf)
+;;; init-recentf.el ends here
