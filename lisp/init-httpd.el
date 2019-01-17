3bdbcb7b342c6598be6d520dee2f20de417024a5
wangyuehong <wangyuehong@gmail.com>
2019-01-17 23:19:57 +0900

    update httpd

--- lisp/init-httpd.el
+++ lisp/init-httpd.el
@@ -1,20 +1,15 @@
-(require-package 'simple-httpd)
+;;; init-httpd.el --- highlight configurations. -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
 
-;; do NOT start elnode-web-server by default, prefer the manual way
-
-;; per project setup may be better
-;; (setq httpd-root "/var/www")
-(setq httpd-port 5678)
-(defun httpd-restart-now ()
-  (interactive)
-  (httpd-stop)
-  (httpd-start)
-  (message "http://localhost:%d/ at %s restarted"
-           httpd-port httpd-root))
-
-(defun httpd-restart-at-default-directory ()
-  (interactive)
+(use-package simple-httpd
+  :defer t
+  :commands(httpd-start httpd-stop)
+  :init
+  (setq httpd-port 5678)
+  ;; (setq httpd-root "/var/www")
   (setq httpd-root default-directory)
-  (httpd-restart-now))
+  )
 
 (provide 'init-httpd)
+;;; init-httpd.el ends here
