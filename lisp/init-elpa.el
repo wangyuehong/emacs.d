cc1ed1411d0d8912d200cb12d69ea157ec884adc
wangyuehong <wangyuehong@gmail.com>
2019-01-20 00:09:26 +0900

    update init-elpa

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -12,11 +12,9 @@
 
 
 ;;; Standard package repositories
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  )
+;; (add-to-list 'package-archives
+;;              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
 ;;; On-demand installation of packages
 
