4110e51cf63fe196a1c4120538497ea2069b112a
wangyuehong <wangyuehong@gmail.com>
2019-02-02 16:27:37 +0900

    init-keybind uses use-package

--- lisp/init-keybind.el
+++ lisp/init-keybind.el
@@ -1,22 +1,24 @@
 ;;; init-keybind.el --- Load the full configuration -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
-(require-package 'key-chord)
-
-(require 'key-chord)
-(key-chord-mode t)
+(use-package key-chord
+  :config
+  (key-chord-mode t)
+  )
 
 (require 'smartchr)
 
-(require-package 'move-text)
-(global-set-key (kbd "M-k") 'move-text-up)
-(global-set-key (kbd "M-j") 'move-text-down)
+(use-package move-text
+  :bind (("M-k" . move-text-up)
+         ("M-j" . move-text-down))
+  )
 
 (define-key minibuffer-local-map (kbd "C-h") 'left-char)
 (define-key minibuffer-local-map (kbd "C-l") 'right-char)
 
-(require-package 'which-key)
-(add-hook 'after-init-hook 'which-key-mode)
+(use-package which-key
+  :hook (after-init . which-key-mode)
+  )
 
 (provide 'init-keybind)
 
