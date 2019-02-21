287f85f384b0a536cb27173e05890d1ced94c1d6
wangyuehong <wangyuehong@gmail.com>
2019-02-21 23:39:04 +0900

    update init-lsp

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -10,12 +10,12 @@
   )
 
 (use-package company-lsp
-  :after lsp-mode
+  :after lsp-mode company
   :bind (("<backtab>" . company-lsp))
   :init
   (setq company-lsp-cache-candidates t)
-  ;; :config
-  ;; (push 'company-lsp company-backends)
+  :config
+  (push '(company-lsp company-dabbrev-code :separate) company-backends)
   )
 
 (provide 'init-lsp)
