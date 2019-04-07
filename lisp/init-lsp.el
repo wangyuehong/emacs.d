4d0465f1f9bdc01c8791f784ddfff7e28f74761e
wangyuehong <wangyuehong@gmail.com>
2019-04-08 00:02:24 +0900

    update lsp-ui

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -18,17 +18,24 @@
 
 (use-package lsp-ui
   :commands lsp-ui-mode
+  :custom-face
+  (lsp-ui-doc-background ((t (:background "brightblack"))))
   :init
   (setq lsp-ui-flycheck-enable t
         lsp-ui-doc-enable nil
+        lsp-ui-doc-header t
+        lsp-ui-doc-include-signature t
+
+        lsp-ui-sideline-enable t
+        lsp-ui-sideline-ignore-duplicate t
         )
   )
 
 (use-package company-lsp
   :commands company-lsp
   :bind (("<backtab>" . company-lsp))
-  ;; :init
-  ;; (setq company-lsp-cache-candidates t)
+  :init
+  (setq company-lsp-cache-candidates 'auto)
   :config
   (push '(company-lsp company-dabbrev :separate) company-backends)
   )
