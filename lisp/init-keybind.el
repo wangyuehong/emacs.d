6678397a1119d749382d1b6ce4ede32031165bf8
wangyuehong <wangyuehong@gmail.com>
2020-04-29 18:22:28 +0900

    demand smartchr

--- lisp/init-keybind.el
+++ lisp/init-keybind.el
@@ -2,7 +2,7 @@
 
 (use-package key-chord :config (key-chord-mode t))
 
-(use-package smartchr :load-path "site-lisp/emacs-smartchr")
+(use-package smartchr :load-path "site-lisp/emacs-smartchr" :demand t)
 
 (define-key minibuffer-local-map (kbd "C-h") 'left-char)
 (define-key minibuffer-local-map (kbd "C-l") 'right-char)
