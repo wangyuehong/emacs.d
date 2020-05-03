48046490f298bbe7564999e9108450efa6a7f9de
wangyuehong <wangyuehong@gmail.com>
2020-05-03 21:13:55 +0900

    reformat init-lsp

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -9,22 +9,22 @@
   :init
   (setq read-process-output-max (* 1024 1024)) ;; 1MB
   (setq lsp-auto-guess-root t
-   lsp-log-io nil
-   lsp-client-packages '(lsp-clients lsp-go)
-   lsp-diagnostic-package :none
-   lsp-keep-workspace-alive nil
-   lsp-enable-indentation nil
-   lsp-enable-snippet nil
-   lsp-enable-folding nil
-   lsp-enable-links nil
-   lsp-enable-completion-at-point nil
-   lsp-enable-symbol-highlighting nil
-   lsp-enable-on-type-formatting nil
-   lsp-flycheck-live-reporting nil
-   lsp-restart 'auto-restart
-   lsp-enable-file-watchers nil
-   lsp-gopls-hover-kind "NoDocumentation"
-   ))
+        lsp-log-io nil
+        lsp-client-packages '(lsp-go)
+        lsp-diagnostic-package :none
+        lsp-keep-workspace-alive nil
+        lsp-enable-indentation nil
+        lsp-enable-snippet nil
+        lsp-enable-folding nil
+        lsp-enable-links nil
+        lsp-enable-completion-at-point nil
+        lsp-enable-symbol-highlighting nil
+        lsp-enable-on-type-formatting nil
+        lsp-flycheck-live-reporting nil
+        lsp-restart 'auto-restart
+        lsp-enable-file-watchers nil
+        lsp-gopls-hover-kind "NoDocumentation"
+        ))
 
 (use-package lsp-ui
   :hook (lsp-mode . lsp-ui-mode)
