0502dfbc251a9434f2220a6e984df3bb41987798
wangyuehong <wangyuehong@gmail.com>
2020-05-03 14:15:37 +0900

    replace highlight-symbol with symbol-overlay

--- lisp/init-highlight.el
+++ lisp/init-highlight.el
@@ -1,13 +1,27 @@
 ;; -*- coding: utf-8; lexical-binding: t; -*-
 
-(use-package highlight-symbol
+(use-package symbol-overlay
   :diminish
+  :bind (("M-i" . symbol-overlay-put)
+         ("M-n" . symbol-overlay-jump-next)
+         ("M-p" . symbol-overlay-jump-prev)
+         :map symbol-overlay-map
+         ("c" . symbol-overlay-remove-all)
+         )
+  :hook ((prog-mode yaml-mode) . symbol-overlay-mode)
   :init
-  (setq highlight-symbol-idle-delay 0.5)
-  (setq highlight-symbol-colors (quote ("#5c5cff" "#ff0000" "#00ff00" "#ff00ff" "#ffff00")))
-  :hook
-  ((prog-mode yaml-mode) . highlight-symbol-mode)
-  ((prog-mode yaml-mode) . highlight-symbol-nav-mode))
+  (setq symbol-overlay-idle-time 0.1)
+  :custom-face
+  (symbol-overlay-default-face ((t (:inherit highlight bold))))
+  (symbol-overlay-face-1 ((t (:background "brightblue" :inverse-video t))))
+  (symbol-overlay-face-2 ((t (:background "brightgreen" :inverse-video t))))
+  (symbol-overlay-face-3 ((t (:background "brightred" :inverse-video t))))
+  (symbol-overlay-face-4 ((t (:background "brightmagenta" :inverse-video t))))
+  (symbol-overlay-face-5 ((t (:background "cyan" :inverse-video t))))
+  (symbol-overlay-face-6 ((t (:background "brightyellow" :inverse-video t))))
+  (symbol-overlay-face-7 ((t (:background "whiteblack" :inverse-video t))))
+  (symbol-overlay-face-8 ((t (:background "magenta" :inverse-video t))))
+  )
 
 (use-package paren
   :ensure nil
