886ebf225a6d962642719b4879e57d1c06ed1070
wangyuehong <wangyuehong@gmail.com>
2020-05-17 11:36:31 +0900

    ignore some buffers in ivy

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -3,14 +3,13 @@
 (use-package counsel
   :diminish ivy-mode counsel-mode
   :bind (("C-x j j" . counsel-bookmark)
-
          :map ivy-minibuffer-map
          ("<return>" . ivy-alt-done)
          ("C-k" . ivy-previous-line)
          ("C-j" . ivy-next-line)
          ("C-b" . ivy-scroll-down-command)
          ("C-f" . ivy-scroll-up-command)
-
+         ("M-j" . ivy-immediate-done)
          :map ivy-switch-buffer-map
          ("C-k" . ivy-previous-line)
          )
@@ -29,6 +28,16 @@
   (ivy-count-format "(%d/%d) ")
   (ivy-on-del-error-function nil)
   (ivy-initial-inputs-alist nil)
+  :config
+  (defun d/ignore-dired-buffers (str)
+    "Return non-nil if STR names a Dired buffer. This function is intended for use with `ivy-ignore-buffers'."
+    (let ((buf (get-buffer str)))
+      (and buf (eq (buffer-local-value 'major-mode buf) 'dired-mode))))
+  (add-to-list 'ivy-ignore-buffers #'d/ignore-dired-buffers)
+  (add-to-list 'ivy-ignore-buffers "\\*Messages\\*")
+  (add-to-list 'ivy-ignore-buffers "\\*Help\\*")
+  (add-to-list 'ivy-ignore-buffers "\\*Ibuffer\\*")
+  (add-to-list 'ivy-ignore-buffers "\\*helm lsgit\\*")
   )
 
 (use-package amx
