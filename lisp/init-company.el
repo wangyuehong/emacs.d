ac873be7213d3f7016875800a317263cbe1c3297
wangyuehong <wangyuehong@gmail.com>
2020-09-13 20:34:13 +0900

    update company

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -4,9 +4,8 @@
   :diminish
   :hook ((prog-mode yaml-mode) . company-mode)
   :commands company-cancel
-  :bind (("M-/" . company-complete)
-         ("C-M-i" . company-complete)
-         :map company-mode-map
+  :bind (:map company-mode-map
+         ([remap completion-at-point] . company-complete)
          ("<backtab>" . company-yasnippet)
          :map company-active-map
          ("C-k" . company-select-previous)
@@ -15,23 +14,27 @@
          ("C-f" . company-next-page)
          ("<tab>" . company-complete-common-or-cycle)
          ("<backtab>" . my-company-yasnippet)
-         ("<return>" . company-complete-selection)
-         ("RET" . company-complete-selection)
          :map company-search-map
          ("C-k" . company-select-previous)
          ("C-j" . company-select-next))
   :custom
-  (company-tooltip-align-annotations t)
-  (company-tooltip-limit 12)
   (company-echo-delay 0)
   (company-idle-delay 0)
+  (company-tooltip-limit 15)
   (company-require-match nil)
-  (company-dabbrev-ignore-invisible t)
+  (company-minimum-prefix-length 1)
+  (company-tooltip-align-annotations t)
+
   (company-dabbrev-other-buffers t)
   (company-dabbrev-ignore-case nil)
   (company-dabbrev-downcase nil)
-  (company-minimum-prefix-length 1)
+  (company-dabbrev-ignore-invisible t)
+  (company-dabbrev-code-everywhere t)
   (company-transformers '(company-sort-by-backend-importance))
+  (company-backends '(company-capf
+                      company-files
+                      (company-dabbrev-code company-keywords)
+                      company-dabbrev))
 
   :init
   (defun my-company-yasnippet ()
@@ -39,12 +42,6 @@
     (interactive)
     (company-cancel)
     (call-interactively 'company-yasnippet))
-
-  :config
-  (setq company-backends (delete 'company-xcode company-backends))
-  (setq company-backends (delete 'company-bbdb company-backends))
-  (setq company-backends (delete 'company-eclim company-backends))
-  (setq company-backends (delete 'company-oddmuse company-backends))
   )
 
 (use-package company-prescient :init (company-prescient-mode 1))
