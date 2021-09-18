9208d61853bc779ac6666b64c97466ef19d769f9
wangyuehong <wangyuehong@gmail.com>
2021-09-18 20:39:13 +0900

    refactor flyspell

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -7,19 +7,11 @@
   :hook
   ((text-mode outline-mode) . flyspell-mode)
   (prog-mode . flyspell-prog-mode)
-  (flyspell-mode . (lambda ()
-                     (dolist (key '("C-;" "C-," "C-."))
-                       (unbind-key key flyspell-mode-map))))
+
   :custom
   (flyspell-issue-message-flag nil)
   (ispell-program-name "aspell")
-  (ispell-extra-args '("--sug-mode=ultra" "--lang=en_US" "--run-together"))
-  )
-
-;; (use-package flyspell-lazy
-;;   :after flyspell
-;;   :config (flyspell-lazy-mode 1)
-;;   )
+  (ispell-extra-args '("--sug-mode=ultra" "--lang=en_US" "--run-together")))
 
 (use-package youdao-dictionary)
 
