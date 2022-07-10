ef6e08385fdc0963a21262f23cca5204b40ab4df
wangyuehong <wangyuehong@gmail.com>
2022-07-10 15:50:41 +0900

    Revert "update general config"
    This reverts commit d58481d6e5019a2edb82ac2281f114d9add9c767.

--- lisp/init-general.el
+++ lisp/init-general.el
@@ -5,33 +5,39 @@
   :config
   (general-evil-setup t)
   (general-define-key
+   :states '(normal visual)
+   :prefix ","
+   ","  'diff-hl-hydra/body
+   "A"  'rg
+   "a"  'rg-project
+   "c"  'evilnc-comment-or-uncomment-lines
+   "d"  'delete-trailing-whitespace
+   "e"  'evil-iedit-state/iedit-mode
+   "K"  'kill-other-buffers
+   "k"  'kill-buffer-and-window
+   "q"  'quickrun
+   "r"  'helm-show-kill-ring
+   "W"  'save-some-buffers
+   "w"  'save-buffer
+   )
+
+  (general-define-key
    ;; :states '(normal motion insert emacs)
    :states '(normal visual emacs)
    :keymaps 'override
    ;; :non-normal-prefix "C-SPC"
    :prefix "SPC"
    "!"  'shell-command
-   ","  'diff-hl-hydra/body
-   "a"  'rg-project
-   "A"  'rg
-   "c"  'evilnc-comment-or-uncomment-lines
-   "C"  'calculator
-   "d"  'delete-trailing-whitespace
-   "e"  'evil-iedit-state/iedit-mode
+   "C"  'calendar
+   "c"  'calculator
    "f"  'flycheck-list-errors
+   "p"  'paradox-upgrade-packages
    "g"  'magit-status
    "h"  'symbol-overlay-put
    "i"  'helm-imenu
-   "k"  'kill-buffer-and-window
-   "K"  'kill-other-buffers
    "l"  'helm-browse-project
    "o"  'helm-occur
-   "p"  'paradox-upgrade-packages
-   "q"  'quickrun
-   "r"  'helm-show-kill-ring
    "t"  'git-timemachine
-   "w"  'save-buffer
-   "W"  'save-some-buffers
    "x"  'helm-M-x
    "y"  'youdao-dictionary-search-at-point+
    )
