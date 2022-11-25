3459d3fac65ff6723aa088e2281dd967dfd18116
wangyuehong <wangyuehong@gmail.com>
2022-11-26 00:07:25 +0900

    drop general key ,

--- lisp/init-general.el
+++ lisp/init-general.el
@@ -6,40 +6,36 @@
   (general-evil-setup t)
   (general-define-key
    :states '(normal visual)
-   :prefix ","
-   ","  'diff-hl-hydra/body
-   "A"  'rg
-   "a"  'rg-project
-   "c"  'evilnc-comment-or-uncomment-lines
-   "d"  'delete-trailing-whitespace
-   "e"  'evil-iedit-state/iedit-mode
-   "K"  'kill-other-buffers
-   "k"  'kill-buffer-and-window
-   "q"  'quickrun
-   "r"  'helm-show-kill-ring
-   "W"  'save-some-buffers
-   "w"  'save-buffer
-   )
-
-  (general-define-key
-   ;; :states '(normal motion insert emacs)
-   :states '(normal visual emacs)
    :keymaps 'override
    ;; :non-normal-prefix "C-SPC"
    :prefix "SPC"
    "!"  'shell-command
-   "C"  'calendar
-   "c"  'calculator
+   "-"  'evil-window-split
+   "|"  'evil-window-vsplit
+   ","  'diff-hl-hydra/body
+   "a"  'rg-project
+   "A"  'rg
+   "b"  'helm-mini
+   "c"  'evilnc-comment-or-uncomment-lines
+   "d"  'osx-dictionary-search-word-at-point
+   "e"  'evil-iedit-state/iedit-mode
    "f"  'flycheck-list-errors
-   "p"  'paradox-upgrade-packages
    "g"  'magit-status
    "h"  'symbol-overlay-put
    "i"  'helm-imenu
+   "j"  'helm-bookmarks
+   "k"  'kill-buffer-and-window
+   "K"  'kill-other-buffers
    "l"  'helm-browse-project
-   "o"  'helm-occur
+   "O"  'helm-occur
+   "o"  'evil-window-next
+   "p"  'paradox-upgrade-packages
+   "q"  'quickrun
+   "r"  'helm-show-kill-ring
    "t"  'git-timemachine
+   "w"  'save-buffer
+   "W"  'save-some-buffers
    "x"  'helm-M-x
-   "y"  'youdao-dictionary-search-at-point+
    )
 )
 (provide 'init-general)
--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -13,6 +13,6 @@
   (ispell-program-name "aspell")
   (ispell-extra-args '("--sug-mode=ultra" "--lang=en_US" "--run-together")))
 
-(use-package youdao-dictionary)
+(use-package osx-dictionary)
 
 (provide 'init-misc)
