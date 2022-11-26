b5e2604fc861af02dc5d766560e07e7956ca7a8d
wangyuehong <wangyuehong@gmail.com>
2022-11-26 22:09:24 +0900

    add tabnine

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -20,14 +20,14 @@
   (company-tooltip-limit 15)
   (company-minimum-prefix-length 1)
   (company-tooltip-align-annotations t)
-
+  (company-show-numbers t)
   (company-dabbrev-other-buffers t)
   (company-dabbrev-ignore-case nil)
   (company-dabbrev-downcase nil)
   (company-dabbrev-ignore-invisible t)
   (company-dabbrev-code-everywhere t)
   (company-transformers '(company-sort-by-backend-importance))
-  (company-backends '((company-capf :separate company-yasnippet :with company-dabbrev)
+  (company-backends '((company-capf :separate company-yasnippet :with company-tabnine)
                       (company-dabbrev-code company-keywords company-files)
                       ))
 
@@ -78,5 +78,35 @@
   )
 
 (use-package company-prescient :init (company-prescient-mode 1))
+
+(use-package company-tabnine
+  :after (company)
+  :config
+  (with-eval-after-load 'company
+    ;; (add-to-list 'company-backends 'company-tabnine)
+    ;; (add-to-list 'company-backends '(company-lsp :with company-tabnine :separate))
+
+    (defun company//sort-by-tabnine (candidates)
+      (if (or (functionp company-backend)
+              (not (and (listp company-backend) (memq 'company-tabnine company-backend))))
+          candidates
+        (let ((candidates-table (make-hash-table :test #'equal))
+              candidates-1
+              candidates-2)
+          (dolist (candidate candidates)
+            (if (eq (get-text-property 0 'company-backend candidate)
+                    'company-tabnine)
+                (unless (gethash candidate candidates-table)
+                  (push candidate candidates-2))
+              (push candidate candidates-1)
+              (puthash candidate t candidates-table)))
+          (setq candidates-1 (nreverse candidates-1))
+          (setq candidates-2 (nreverse candidates-2))
+          (nconc (seq-take candidates-1 2)
+                 (seq-take candidates-2 2)
+                 (seq-drop candidates-1 2)
+                 (seq-drop candidates-2 2)))))
+
+    (add-to-list 'company-transformers 'company//sort-by-tabnine t)))
 
 (provide 'init-company)
