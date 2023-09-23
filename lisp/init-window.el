66f82eafec26ae359d7c4a47cf9ec061960667c9
yuehong.wang <yuehong.wang@dena.jp>
2023-09-23 15:16:21 +0900

    config shackle

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -19,15 +19,16 @@
     :hook (after-init . shackle-mode)
     :custom
     (shackle-default-rule nil)
+    (shackle-default-size 0.5)
     (shackle-default-alignment 'below)
     (shackle-rules '((flycheck-error-list-mode :select t :align t :size 0.4)
                      (magit-status-mode :select t :inhibit-window-quit t :same t)
+                     (magit-log-mode    :select t :inhibit-window-quit t :same t)
+                     ("*quickrun*"      :select t :inhibit-window-quit t :same t)
                      (help-mode :select t :align t :size 0.4)
                      (compilation-mode :select t :align t :size 0.4)
                      (go-test-mode :select t :align t :size 0.4)
-                     (rg-mode :select t :align right :size 0.4)
-                     ))
-    )
+                     (rg-mode :select t :align right :size 0.4))))
 
 (use-package ibuffer
   :ensure nil
