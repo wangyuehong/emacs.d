27884b45e0da28c493fa575fe0dce636c6cb5f97
wangyuehong <wangyuehong@gmail.com>
2023-09-23 13:22:02 +0900

    refactor company and yasnippet

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -21,58 +21,13 @@
         company-require-match nil
         company-dabbrev-ignore-case nil
         company-dabbrev-downcase nil
-        company-global-modes '(not erc-mode message-mode help-mode
-                                   gud-mode eshell-mode shell-mode)
+        company-global-modes '(not erc-mode message-mode help-mode gud-mode eshell-mode shell-mode)
         company-format-margin-function #'company-text-icons-margin
         company-frontends '(company-pseudo-tooltip-frontend ; show tooltip even for single candidate
                             company-echo-metadata-frontend)
         company-transformers '(company-sort-by-backend-importance)
-        company-backends '((company-capf :separate company-yasnippet)
-                           (company-dabbrev-code company-keywords company-files)))
-  :config
-  (with-no-warnings
-    ;; `yasnippet' integration
-    (with-eval-after-load 'yasnippet
-      (defun my-company-yasnippet ()
-        "Hide the current completeions and show snippets."
-        (interactive)
-        (company-cancel)
-        (call-interactively 'company-yasnippet))
-
-      (defun company-backend-with-yas (backend)
-        "Add `yasnippet' to company backend."
-        (if (and (listp backend) (member 'company-yasnippet backend))
-            backend
-          (append (if (consp backend) backend (list backend))
-                  '(:with company-yasnippet))))
-
-      (defun my-company-enbale-yas (&rest _)
-        "Enable `yasnippet' in `company'."
-        (setq company-backends (mapcar #'company-backend-with-yas company-backends)))
-
-      (defun my-lsp-fix-company-capf ()
-        "Remove redundant `comapny-capf'."
-        (setq company-backends
-              (remove 'company-backends (remq 'company-capf company-backends))))
-      (advice-add #'lsp-completion--enable :after #'my-lsp-fix-company-capf)
-
-      (defun my-company-yasnippet-disable-inline (fn cmd &optional arg &rest _ignore)
-        "Enable yasnippet but disable it inline."
-        (if (eq cmd  'prefix)
-            (when-let ((prefix (funcall fn 'prefix)))
-              (unless (memq (char-before (- (point) (length prefix)))
-                            '(?. ?< ?> ?\( ?\) ?\[ ?{ ?} ?\" ?' ?`))
-                prefix))
-          (progn
-            (when (and (bound-and-true-p lsp-mode)
-                       arg (not (get-text-property 0 'yas-annotation-patch arg)))
-              (let* ((name (get-text-property 0 'yas-annotation arg))
-                     (snip (format "%s (Snippet)" name))
-                     (len (length arg)))
-                (put-text-property 0 len 'yas-annotation snip arg)
-                (put-text-property 0 len 'yas-annotation-patch t arg)))
-            (funcall fn cmd arg))))
-      (advice-add #'company-yasnippet :around #'my-company-yasnippet-disable-inline))))
+        company-backends '((company-capf :with company-yasnippet :separate)
+                           (company-dabbrev-code company-keywords company-files))))
 
 (use-package prescient
   :commands prescient-persist-mode
@@ -114,7 +69,7 @@
     "Hook to attach to `lsp-after-open'."
     (setq-local company-tabnine-max-num-results 5)
     (add-to-list 'company-transformers 'company//sort-by-tabnine t)
-    (add-to-list 'company-backends '(company-tabnine :separate company-yasnippet :separate company-capf)))
+    (add-to-list 'company-backends '(company-tabnine :with company-yasnippet company-capf :separate)))
   (defun company-tabnine-toggle (&optional enable)
     "Enable/Disable TabNine. If ENABLE is non-nil, definitely enable it."
     (interactive)
@@ -125,7 +80,7 @@
           (when (bound-and-true-p lsp-mode) (lsp-after-open-tabnine))
           (message "TabNine enabled."))
       (setq company-backends (delete 'company-tabnine company-backends))
-      (setq company-backends (delete '(company-tabnine :separate company-yasnippet :separate company-capf) company-backends))
+      (setq company-backends (delete '(company-tabnine :with company-yasnippet company-capf :separate) company-backends))
       (remove-hook 'lsp-after-open-hook #'lsp-after-open-tabnine)
       (company-tabnine-kill-process)
       (message "TabNine disabled."))))
