31a095bf0daf6563b5a47f8c78cb61295eb2a021
yuehong.wang <yuehong.wang@dena.jp>
2023-09-23 15:03:16 +0900

    update dired config

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -9,9 +9,9 @@
   (dired-recursive-deletes 'always)
   (dired-recursive-copies 'always)
   (dired-hide-details-hide-symlink-targets nil)
-  (dired-listing-switches "-Ahlv")
+  (dired-listing-switches "-AFhlv")
   (dired-dwim-target t)
-  ;; (dired-kill-when-opening-new-dired-buffer t)
+  (dired-kill-when-opening-new-dired-buffer t)
   (dired-clean-confirm-killing-deleted-buffers nil)
   (dired-auto-revert-buffer #'dired-directory-changed-p)
   :config
