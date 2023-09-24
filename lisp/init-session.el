e69c8a969d01c2428a7231c2c0877229ca9527e7
wangyuehong <wangyuehong@gmail.com>
2023-09-24 13:22:30 +0900

    add desktop config

--- lisp/init-session.el
+++ lisp/init-session.el
@@ -1,23 +1,29 @@
 ;; -*- coding: utf-8; lexical-binding: t; -*-
 
+(use-package desktop
+  :ensure nil
+  :init
+  (desktop-save-mode 1)
+  :custom
+  (desktop-load-locked-desktop t)
+  (desktop-auto-save-timeout 60))
+
 (use-package recentf
   :ensure nil
   :hook (after-init . recentf-mode)
   :custom
-  (recentf-max-saved-items 64)
+  (recentf-max-saved-items 256)
+  (recentf-auto-cleanup 'never)
   (recentf-save-file (expand-file-name ".recentf" user-emacs-directory))
-  (recentf-exclude
-   '("^/private/tmp/"
-     "^/var/folders/"
-     "^/tmp/"
-     "bookmarks"
-     "/ssh\\(x\\)?:"
-     "/su\\(do\\)?:"
-     "^/usr/include/"
-     "/TAGS\\'"
-     "/G?TAGS$"
-     "COMMIT_EDITMSG\\'")))
-
+  (recentf-exclude '("^/private/tmp/"
+                     "^/var/folders/"
+                     "^/tmp/"
+                     "/ssh\\(x\\)?:"
+                     "/su\\(do\\)?:"
+                     "^/usr/include/"
+                     "/TAGS\\'"
+                     "/G?TAGS$"
+                     "COMMIT_EDITMSG\\'")))
 
 (use-package saveplace
   :ensure nil
