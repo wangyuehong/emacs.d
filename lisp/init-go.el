e8d73d4dbd76384390d2b3bf5e1e3645fe7c39f4
wangyuehong <wangyuehong@gmail.com>
2023-10-01 01:33:53 +0900

    drop unused go packages

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -4,9 +4,8 @@
   :bind (:map go-mode-map
               ("C-c i" . go-import-add))
   :config
-  (use-package go-dlv)
-  (use-package go-impl)
-  (use-package go-fill-struct)
+  (use-package go-tag)
+  (use-package go-gen-test)
   (use-package go-snippets)
   (use-package gotest
     :bind (:map go-mode-map
@@ -15,15 +14,6 @@
 	:config
 	(setq go-test-args "-failfast -race")
     :custom
-    (go-test-verbose t))
-
-  (use-package go-gen-test
-    :bind (:map go-mode-map
-                ("C-c t g" . go-gen-test-dwim)))
-
-  (use-package go-tag
-    :bind (:map go-mode-map
-                ("C-c t a" . go-tag-add)
-                ("C-c t r" . go-tag-remove))))
+    (go-test-verbose t)))
 
 (provide 'init-go)
