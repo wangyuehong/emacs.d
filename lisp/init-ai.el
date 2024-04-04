d1dddd97413fcaa72f7535909ce3c75a36f057d5
wangyuehong <wangyuehong@gmail.com>
2024-04-04 19:39:27 +0900

    delete tabnine

--- lisp/init-ai.el
+++ lisp/init-ai.el
@@ -20,15 +20,4 @@
   :custom
   (copilot-log-max 0))
 
-(use-package tabnine
-  ;; :commands (tabnine-start-process)
-  :hook ((kill-emacs . tabnine-kill-process))
-  :config
-  (add-to-list 'completion-at-point-functions #'tabnine-completion-at-point)
-  ;; (tabnine-start-process)
-  :custom
-  (tabnine-max-num-results 5)
-  :custom-face
-  (tabnine-overlay-face ((t (:inherit shadow :foreground "#cd5c5c")))))
-
 (provide 'init-ai)
