53dfe48eb8450e954defbefddc5c7e6b87fb245f
wangyuehong <wangyuehong@gmail.com>
2024-11-03 15:52:05 +0900

    refactor package initialization

--- init.el
+++ init.el
@@ -24,7 +24,6 @@
 
 (update-load-path)
 
-(require 'init-elpa)
 (require 'init-package)
 
 ;;----------------------------------------------------------------------------
@@ -64,3 +63,16 @@
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-vc-selected-packages
+   '((copilot :url "https://github.com/copilot-emacs/copilot.el" :branch "main"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- lisp/init-package.el
+++ lisp/init-package.el
@@ -2,20 +2,27 @@
 ;;; Commentary:
 ;;; Code:
 
-;; Initialize packages
-(package-initialize)
 
-;; Setup `use-package'
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-;; Should set before loading `use-package'
-(setq use-package-always-ensure t
-  use-package-always-defer t
-  use-package-expand-minimally t
-  package-install-upgrade-built-in t
-  use-package-enable-imenu-support t)
+(use-package package
+  :ensure nil
+  :init
+  (setq use-package-always-ensure t
+    use-package-always-defer t
+    use-package-expand-minimally t
+    package-install-upgrade-built-in t
+    use-package-enable-imenu-support t)
+  :config
+  ;; install into separate package dirs for each emacs version
+  (let ((versioned-package-dir
+          (expand-file-name (format "elpa-%s.%s" emacs-major-version emacs-minor-version)
+            user-emacs-directory)))
+    (setq package-user-dir versioned-package-dir))
+  (setq package-archives
+    '(("gnu"    . "https://elpa.gnu.org/packages/")
+       ("melpa"  . "https://melpa.org/packages/")
+       ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
+  (unless (bound-and-true-p package--initialized)
+    (package-initialize)))
 
 ;; Update GPG keyring for GNU ELPA
 (use-package gnu-elpa-keyring-update)
