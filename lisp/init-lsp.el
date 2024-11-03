46b094e8ead3c1860f9a7e9e54623eff579e3fbb
wangyuehong <wangyuehong@gmail.com>
2024-11-03 19:37:04 +0900

    eglot format buffer only if server supports it

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -7,8 +7,9 @@
   (defun my/eglot-organize-imports () (interactive)
          (eglot-code-actions nil nil "source.organizeImports" t))
   (defun my/eglot-setup-hooks () (interactive)
-         (add-hook 'before-save-hook 'my/eglot-organize-imports nil t)
-         (add-hook 'before-save-hook 'eglot-format-buffer nil t))
+    (add-hook 'before-save-hook 'my/eglot-organize-imports nil t)
+    (when (eglot--server-capable :documentFormattingProvider)
+      (add-hook 'before-save-hook 'eglot-format-buffer nil t)))
   (defun my/eglot-capf ()
     (add-to-list 'completion-at-point-functions #'cape-dabbrev))
 
