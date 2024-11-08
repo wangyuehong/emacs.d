c8e807a49657da757d5f0a98ae95f9d8ce9a15e5
wangyuehong <wangyuehong@gmail.com>
2024-11-08 21:41:59 +0900

    use project-find-go-mod only in go-mode

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -2,19 +2,22 @@
 ;;; Commentary:
 ;;; Code:
 
-(defun project-find-go-module (dir)
-  (when-let ((root (locate-dominating-file dir "go.mod")))
-    (cons 'go-module root)))
-
-(cl-defmethod project-root ((project (head go-module)))
-  (cdr project))
-
-(add-hook 'project-find-functions #'project-find-go-module)
-
 (use-package go-mode
+  :preface
+  (defun project-find-go-mod (dir)
+    (when-let ((root (locate-dominating-file dir "go.mod")))
+      (cons 'go-module root)))
+
+  (cl-defmethod project-root ((project (head go-module)))
+    (cdr project))
+
+  (defun my/set-go-project-find-functions ()
+    "Add 'project-find-go-mod' to 'project-find-functions' in go-mode."
+    (add-to-list 'project-find-functions #'project-find-go-mod))
   :bind (:map go-mode-map
           ("C-c i" . go-import-add))
-  :hook (go-mode . eglot-ensure)
+  :hook ((go-mode  . eglot-ensure)
+          (go-mode . my/set-go-project-find-functions))
   :config
   (use-package go-tag)
   (use-package go-gen-test)
