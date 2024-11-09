fab09deb921aefeae1536e10e8e54911bc8dd0f4
wangyuehong <wangyuehong@gmail.com>
2024-11-09 20:18:54 +0900

    chore: remove custom-set-variables and custom-set-faces from init.el

--- init.el
+++ init.el
@@ -65,16 +65,3 @@
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-vc-selected-packages
-   '((copilot :url "https://github.com/copilot-emacs/copilot.el" :branch "main"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
