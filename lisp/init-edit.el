4b0af3ce83bbfd4d6d5300148bb0f6decbab695d
wangyuehong <wangyuehong@gmail.com>
2024-11-16 15:58:47 +0900

    update markdown-mode configuration in init-edit.el

--- lisp/init-edit.el
+++ lisp/init-edit.el
@@ -44,11 +44,9 @@
   :mode ("README\\(?:\\.md\\)?\\'" . gfm-mode)
   :hook (markdown-mode . visual-line-mode)
   :custom
-  (markdown-header-scaling t)
   (markdown-enable-wiki-links t)
   (markdown-italic-underscore t)
   (markdown-asymmetric-header t)
-  (markdown-gfm-uppercase-checkbox t)
   (markdown-fontify-code-blocks-natively t))
 
 (use-package editorconfig
