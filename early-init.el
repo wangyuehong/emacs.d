3a2aec4cdecbc187591d3eae3d8b8e94e80e292b
wangyuehong <wangyuehong@gmail.com>
2024-11-23 21:21:35 +0900

    clean up early-init.el by removing unnecessary settings

--- early-init.el
+++ early-init.el
@@ -5,24 +5,10 @@
 ;; Defer garbage collection further back in the startup process
 (setq gc-cons-threshold most-positive-fixnum)
 
-;; Prevent unwanted runtime compilation for gccemacs (native-comp) users;
-;; packages are compiled ahead-of-time when they are installed and site files
-;; are compiled when gccemacs is installed.
-(setq native-comp-jit-compilation nil)
-
 ;; Package initialize occurs automatically, before `user-init-file' is
 ;; loaded, but after `early-init-file'. We handle package
 ;; initialization, so we must prevent Emacs from doing it early!
 (setq package-enable-at-startup nil)
-
-;; `use-package' is builtin since 29.
-;; It must be set before loading `use-package'.
-(setq use-package-enable-imenu-support t)
-
-;; In noninteractive sessions, prioritize non-byte-compiled source files to
-;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
-;; to skip the mtime checks on every *.elc file.
-(setq load-prefer-newer noninteractive)
 
 ;; Inhibit resizing frame
 (setq frame-inhibit-implied-resize t)
@@ -33,7 +19,5 @@
 (push '(vertical-scroll-bars) default-frame-alist)
 (when (featurep 'ns)
   (push '(ns-transparent-titlebar . t) default-frame-alist))
-
-(setq-default mode-line-format nil)
 
 ;;; early-init.el ends here
