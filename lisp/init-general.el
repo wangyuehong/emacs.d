d2a11e351f85e53f18d4fbcc863bb447b8e8ddc6
wangyuehong <wangyuehong@gmail.com>
2024-12-12 20:38:01 +0900

    style(init-general): align keybinding descriptions for consistency

--- lisp/init-general.el
+++ lisp/init-general.el
@@ -24,19 +24,19 @@
     "g" 'magit-status
     "h" 'symbol-overlay-put
     "i" 'imenu
-    "l" '(consult-ls-git                    :wk "ls-git")
-    "o" '(my/consult-line                   :wk "occur")
+    "l" '(consult-ls-git              :wk "ls-git")
+    "o" '(my/consult-line             :wk "occur")
     "q" 'quickrun
-    "r" '(consult-yank-from-kill-ring       :wk "yank-from-kill-ring")
+    "r" '(consult-yank-from-kill-ring :wk "yank-from-kill-ring")
     "w" 'save-buffer
-    "x" '(execute-extended-command          :wk "M-x")
+    "x" '(execute-extended-command    :wk "M-x")
 
     "t"  '(:wk "toggles")
-    "tl" '(toggle-truncate-lines       :wk "toggle-truncate-lines")
-    "tm" '(xterm-mouse-mode            :wk "toggle-mouse-mode")
-    "tn" '(display-line-numbers-mode   :wk "toggle-display-line-numbers")
-    "tp" '(electric-pair-mode          :wk "toggle-electric-pair-mode")
-    "tt" '(git-timemachine-toggle      :wk "toggle-git-timemachine")
+    "tl" '(toggle-truncate-lines     :wk "truncate-lines")
+    "tm" '(xterm-mouse-mode          :wk "xterm-mouse-mode")
+    "tn" '(display-line-numbers-mode :wk "display-line-numbers")
+    "tp" '(electric-pair-mode        :wk "electric-pair-mode")
+    "tt" '(git-timemachine-toggle    :wk "git-timemachine")
     ))
 
 (provide 'init-general)
