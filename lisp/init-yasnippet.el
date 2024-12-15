92b71f1c86c5613e52da434b1d724098b213c51b
wangyuehong <wangyuehong@gmail.com>
2024-12-15 13:29:33 +0900

    feat(yasnippet): add key bindings and new snippets for Go and fundamental modes

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -3,7 +3,12 @@
 ;;; Code:
 
 (use-package yasnippet
-  :hook (after-init . yas-global-mode))
+  :defines yas-keymap
+  :hook (after-init . yas-global-mode)
+  :bind
+  (:map yas-keymap
+    ("C-f" . yas-next-field-or-maybe-expand)
+    ("C-b" . yas-prev-field)))
 
 (use-package yasnippet-snippets
   :after yasnippet)
