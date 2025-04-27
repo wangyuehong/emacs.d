08fea09c36a71e56996958002acb86952e00e564
wangyuehong <wangyuehong@gmail.com>
2025-04-27 23:31:35 +0900

    refactor(keybind): improve `my/keyboard-quit-dwim` logic for better mode handling

--- lisp/init-keybind.el
+++ lisp/init-keybind.el
@@ -40,30 +40,33 @@ The DWIM behaviour of this command is as follows:
 - When the Completions buffer is selected, close it.
 - When in `vterm-copy-mode', close it.
 - When in `vterm-mode', call `vterm-reset-cursor-point'.
-- When in `copilot-mode', call function `copilot-chat-goto-input'.
+- When in `copilot-chat-prompt-mode', call `copilot-chat-goto-input'.
 - When in `gptel-mode', call `gptel-end-of-response'.
 - When in `comint-mode', call `comint-goto-process-mark'.
 - In every other case use the regular `keyboard-quit'."
   (interactive)
   (cond
-   ((region-active-p)
-    (keyboard-quit))
-   ((derived-mode-p 'completion-list-mode)
-    (delete-completion-window))
-   ((> (minibuffer-depth) 0)
-    (abort-recursive-edit))
-   ((bound-and-true-p vterm-copy-mode)
-    (vterm-copy-mode -1))
-   ((derived-mode-p 'vterm-mode)
-    (vterm-reset-cursor-point))
-   ((and (boundp 'copilot-mode) copilot-mode)
-    (copilot-chat-goto-input))
-   ((and (boundp 'gptel-mode) gptel-mode)
-    (gptel-end-of-response))
-   ((derived-mode-p 'comint-mode)
-    (comint-goto-process-mark))
-   (t
-    (keyboard-quit))))
+    ((region-active-p)
+      (deactivate-mark))
+    ((derived-mode-p 'completion-list-mode)
+      (delete-completion-window))
+    ((minibufferp)
+      (abort-recursive-edit))
+    ((bound-and-true-p vterm-copy-mode)
+      (vterm-copy-mode -1))
+    ((derived-mode-p 'vterm-mode)
+      (vterm-reset-cursor-point))
+    ((bound-and-true-p copilot-chat-prompt-mode)
+      (copilot-chat-goto-input))
+    ((and (bound-and-true-p copilot-chat-markdown-poly-mode)
+       (eq major-mode 'markdown-view-mode))
+      (copilot-chat-goto-input))
+    ((and (boundp 'gptel-mode) gptel-mode)
+      (gptel-end-of-response))
+    ((derived-mode-p 'comint-mode)
+      (comint-goto-process-mark))
+    (t
+      (keyboard-quit))))
 
 (define-key global-map (kbd "C-g") #'my/keyboard-quit-dwim)
 
