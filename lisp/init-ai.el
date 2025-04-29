ca6913bfd91d49b4db440880140e5a1df7101c2e
wangyuehong <wangyuehong@gmail.com>
2025-04-29 23:16:37 +0900

    refactor(ai): remove unused tool definitions and streamline tool creation
    This commit removes commented-out tool definitions and redundant code in `init-ai.el`. The changes simplify the file by relying on `llm-tool-collection-get-all` for tool creation, improving maintainability and readability.

--- lisp/init-ai.el
+++ lisp/init-ai.el
@@ -64,119 +64,6 @@
           :branch "main"
           :rev :newest))
 
-  (mapcar (apply-partially #'apply #'gptel-make-tool)
-    (llm-tool-collection-get-all))
-
-;;   (gptel-make-tool
-;;     :name "read_file"
-;;     :description "Read and display the contents of a file"
-;;     :category "filesystem"
-;;     :function (lambda (filepath)
-;;                 (with-temp-buffer
-;;                   (insert-file-contents (expand-file-name filepath))
-;;                   (buffer-string)))
-;;     :args (list '(:name "filepath"
-;;                    :type string
-;;                    :description "Path to the file to read. Supports relative paths and ~.")))
-
-;;   (gptel-make-tool
-;;     :name "list_directory"
-;;     :description "List the contents of a given directory"
-;;     :category "filesystem"
-;;     :function (lambda (directory)
-;;                 (mapconcat #'identity
-;;                   (directory-files directory)
-;;                   "\n"))
-;;     :args (list '(:name "directory"
-;;                    :type string
-;;                    :description "The path to the directory to list")))
-
-;;   (gptel-make-tool
-;;     :name "make_directory"
-;;     :description "Create a new directory with the given name in the specified parent directory"
-;;     :category "filesystem"
-;;     :function (lambda (parent name)
-;;                 (condition-case nil
-;;                   (progn
-;;                     (make-directory (expand-file-name name parent) t)
-;;                     (format "Directory %s created/verified in %s" name parent))
-;;                   (error (format "Error creating directory %s in %s" name parent))))
-;;     :args (list '(:name "parent"
-;;                    :type string
-;;                    :description "The parent directory where the new directory should be created, e.g. /tmp")
-;;             '(:name "name"
-;;                :type string
-;;                :description "The name of the new directory to create, e.g. testdir")))
-;;   (gptel-make-tool
-;;     :name "create_file"
-;;     :description "Create a new file with the specified content"
-;;     :category "filesystem"
-;;     :function (lambda (path filename content)
-;;                 (let ((full-path (expand-file-name filename path)))
-;;                   (with-temp-buffer
-;;                     (insert content)
-;;                     (write-file full-path))
-;;                   (format "Created file %s in %s" filename path)))
-;;     :args (list '(:name "path"
-;;                    :type string
-;;                    :description "The directory where to create the file")
-;;             '(:name "filename"
-;;                :type string
-;;                :description "The name of the file to create")
-;;             '(:name "content"
-;;                :type string
-;;                :description "The content to write to the file")))
-
-;;   (defun my-gptel--edit_file (file-path file-edits)
-;;     "In FILE-PATH, apply FILE-EDITS with pattern matching and replacing."
-;;     (if (and file-path (not (string= file-path "")) file-edits)
-;;       (with-current-buffer (get-buffer-create "*edit-file*")
-;;         (insert-file-contents (expand-file-name file-path))
-;;         (let ((inhibit-read-only t)
-;;                (case-fold-search nil)
-;;                (file-name (expand-file-name file-path))
-;;                (edit-success nil))
-;;           ;; apply changes
-;;           (dolist (file-edit (seq-into file-edits 'list))
-;;             (when-let ((line-number (plist-get file-edit :line_number))
-;;                         (old-string (plist-get file-edit :old_string))
-;;                         (new-string (plist-get file-edit :new_string))
-;;                         (is-valid-old-string (not (string= old-string ""))))
-;;               (goto-char (point-min))
-;;               (forward-line (1- line-number))
-;;               (when (search-forward old-string nil t)
-;;                 (replace-match new-string t t)
-;;                 (setq edit-success t))))
-;;           ;; return result to gptel
-;;           (if edit-success
-;;             (progn
-;;               ;; show diffs
-;;               (ediff-buffers (find-file-noselect file-name) (current-buffer))
-;;               (format "Successfully edited %s" file-name))
-;;             (format "Failed to edited %s" file-name))))
-;;       (format "Failed to edited %s" file-path)))
-
-;;   (gptel-make-tool
-;;     :name "edit_file"
-;;     :description "Edit file with a list of edits, each edit contains a line-number,
-;; a old-string and a new-string, new-string will replace the old-string at the specified line."
-;;     :category "filesystem"
-;;     :function #'my-gptel--edit_file
-;;     :args (list '(:name "file-path"
-;;                    :type string
-;;                    :description "The full path of the file to edit")
-;;             '(:name "file-edits"
-;;                :type array
-;;                :items (:type object
-;;                         :properties
-;;                         (:line_number
-;;                           (:type integer :description "The line number of the file where edit starts.")
-;;                           :old_string
-;;                           (:type string :description "The old-string to be replaced.")
-;;                           :new_string
-;;                           (:type string :description "The new-string to replace old-string.")))
-;;                :description "The list of edits to apply on the file")))
-
   (gptel-make-tool
     :name "run_command"
     :description "Run a command."
@@ -188,50 +75,6 @@
             '(:name "command"
                :type "string"
                :description "Command to run.")))
-
-;;   (gptel-make-tool
-;;     :name "echo_message"
-;;     :description "Send a message to the *Messages* buffer"
-;;     :category "emacs"
-;;     :function (lambda (text)
-;;                 (message "%s" text)
-;;                 (format "Message sent: %s" text))
-;;     :args (list '(:name "text"
-;;                    :type string
-;;                    :description "The text to send to the messages buffer")))
-
-;;   (defun gptel-read-documentation (symbol)
-;;     "Read the documentation for SYMBOL, which can be a function or variable."
-;;     (let ((sym (intern symbol)))
-;;       (cond
-;;         ((fboundp sym)
-;;           (documentation sym))
-;;         ((boundp sym)
-;;           (documentation-property sym 'variable-documentation))
-;;         (t
-;;           (format "No documentation found for %s" symbol)))))
-
-;;   (gptel-make-tool
-;;     :name "read_documentation"
-;;     :description "Read the documentation for a given function or variable"
-;;     :category "emacs"
-;;     :function #'gptel-read-documentation
-;;     :args (list '(:name "name"
-;;                    :type string
-;;                    :description "The name of the function or variable whose documentation is to be retrieved")))
-
-;;   (gptel-make-tool
-;;     :name "read_buffer"
-;;     :description "Return the contents of an Emacs buffer"
-;;     :category "emacs"
-;;     :function (lambda (buffer)
-;;                 (unless (buffer-live-p (get-buffer buffer))
-;;                   (error "Error: buffer %s is not live" buffer))
-;;                 (with-current-buffer buffer
-;;                   (buffer-substring-no-properties (point-min) (point-max))))
-;;     :args (list '(:name "buffer"
-;;                    :type string
-;;                    :description "The name of the buffer whose contents are to be retrieved")))
 
   (gptel-make-tool
     :name "append_to_buffer"
@@ -250,42 +93,8 @@
                :type string
                :description "The text to append to the buffer.")))
 
-;; (defun codel-edit-buffer (buffer-name old-string new-string)
-;; "In BUFFER-NAME, replace OLD-STRING with NEW-STRING."
-;; (with-current-buffer buffer-name
-;;   (let ((case-fold-search nil))  ;; Case-sensitive search
-;;     (save-excursion
-;;       (goto-char (point-min))
-;;       (let ((count 0))
-;;         (while (search-forward old-string nil t)
-;;           (setq count (1+ count)))
-;;         (if (= count 0)
-;;             (format "Error: Could not find text to replace in buffer %s" buffer-name)
-;;           (if (> count 1)
-;;               (format "Error: Found %d matches for the text to replace in buffer %s" count buffer-name)
-;;             (goto-char (point-min))
-;;             (search-forward old-string)
-;;             (replace-match new-string t t)
-;;             (format "Successfully edited buffer %s" buffer-name))))))))
-
-;;   (gptel-make-tool
-;;     :name "edit_buffer"
-;;     :description "Edits Emacs buffers"
-;;     :category "edit"
-;;     :function #'codel-edit-buffer
-;;     :args '((:name "buffer_name"
-;;               :type string
-;;               :description "Name of the buffer to modify"
-;;               :required t)
-;;              (:name "old_string"
-;;                :type string
-;;                :description "Text to replace (must match exactly)"
-;;                :required t)
-;;              (:name "new_string"
-;;                :type string
-;;                :description "Text to replace old_string with"
-;;                :required t)))
-  )
+  (mapcar (apply-partially #'apply #'gptel-make-tool)
+    (llm-tool-collection-get-all)))
 
 (use-package gptel-quick
   :vc (:url "https://github.com/karthink/gptel-quick" :branch "main" :rev :newest)
