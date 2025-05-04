6835f8126e4c08145bba106c90674a8e28ae2dcc
wangyuehong <wangyuehong@gmail.com>
2025-05-04 23:44:42 +0900

    feat(theme): add support for sanityinc-solarized-light theme in GUI mode
    Added a conditional to load the sanityinc-solarized-light theme when running in a graphical environment. Retained srcery theme for non-GUI environments.

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -3,8 +3,13 @@
 ;;; Code:
 
 (use-package srcery-theme
+  :unless (display-graphic-p)
   :init (load-theme 'srcery t)
   :custom (srcery-invert-region nil))
+
+(use-package color-theme-sanityinc-solarized
+  :if (display-graphic-p)
+  :init (load-theme 'sanityinc-solarized-light t))
 
 (provide 'init-theme)
 ;;; init-theme.el ends here
