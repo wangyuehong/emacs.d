73c559dd266e87e237ca3147dd330fbd4dab206b
wangyuehong <wangyuehong@gmail.com>
2025-07-27 12:56:21 +0900

    refactor(init-im): comment out unused input method configurations
    - Disabled configurations for mozc, pyim, and im-bridge using comments
    - Commented out input method switching functions and related keybindings
    - Preserved existing structure for potential future use

--- lisp/init-im.el
+++ lisp/init-im.el
@@ -2,50 +2,50 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package mozc
-  :if (executable-find "mozc_emacs_helper")
-  :custom
-  (mozc-candidate-style 'echo-area)
-  (default-input-method "japanese-mozc"))
+;; (use-package mozc
+;;   :if (executable-find "mozc_emacs_helper")
+;;   :custom
+;;   (mozc-candidate-style 'echo-area)
+;;   (default-input-method "japanese-mozc"))
 
-(use-package popup)
-(use-package pyim
-  :bind (("M-i" . pyim-toggle-input-ascii)
-          :map pyim-mode-map
-          ("C-f" . pyim-next-page)
-          ("C-b" . pyim-previous-page))
-  :custom
-  (pyim-default-scheme 'quanpin)
-  (pyim-page-length 6)
-  (pyim-page-tooltip 'popup)
-  :config
-  (require 'popup)
-  (use-package pyim-basedict)
-  (setq-default pyim-punctuation-translate-p '(no)) ;; 默认使用半角标点符号
-  (pyim-basedict-enable))
+;; (use-package popup)
+;; (use-package pyim
+;;   :bind (("M-i" . pyim-toggle-input-ascii)
+;;           :map pyim-mode-map
+;;           ("C-f" . pyim-next-page)
+;;           ("C-b" . pyim-previous-page))
+;;   :custom
+;;   (pyim-default-scheme 'quanpin)
+;;   (pyim-page-length 6)
+;;   (pyim-page-tooltip 'popup)
+;;   :config
+;;   (require 'popup)
+;;   (use-package pyim-basedict)
+;;   (setq-default pyim-punctuation-translate-p '(no)) ;; 默认使用半角标点符号
+;;   (pyim-basedict-enable))
 
-(use-package im-bridge
-  :ensure nil ;; site-lisp/im-bridge
-  :hook
-  ((after-init . imb-evil-mode)))
+;; (use-package im-bridge
+;;   :ensure nil ;; site-lisp/im-bridge
+;;   :hook
+;;   ((after-init . imb-evil-mode)))
 
-;; Input method switching functions and keybindings
-(defun my/set-input-method-pyim ()
-  "Set default input method to pyim and activate it."
-  (interactive)
-  (setq default-input-method "pyim")
-  (set-input-method "pyim")
-  (message "Input method set to: pyim"))
+;; ;; Input method switching functions and keybindings
+;; (defun my/set-input-method-pyim ()
+;;   "Set default input method to pyim and activate it."
+;;   (interactive)
+;;   (setq default-input-method "pyim")
+;;   (set-input-method "pyim")
+;;   (message "Input method set to: pyim"))
 
-(defun my/set-input-method-mozc ()
-  "Set default input method to japanese-mozc and activate it."
-  (interactive)
-  (setq default-input-method "japanese-mozc")
-  (set-input-method "japanese-mozc")
-  (message "Input method set to: japanese-mozc"))
+;; (defun my/set-input-method-mozc ()
+;;   "Set default input method to japanese-mozc and activate it."
+;;   (interactive)
+;;   (setq default-input-method "japanese-mozc")
+;;   (set-input-method "japanese-mozc")
+;;   (message "Input method set to: japanese-mozc"))
 
-(global-set-key (kbd "C-c i j") #'my/set-input-method-mozc)
-(global-set-key (kbd "C-c i i") #'my/set-input-method-pyim)
+;; (global-set-key (kbd "C-c i j") #'my/set-input-method-mozc)
+;; (global-set-key (kbd "C-c i i") #'my/set-input-method-pyim)
 
 (provide 'init-im)
 ;;; init-im.el ends here
