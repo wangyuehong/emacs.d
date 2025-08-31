e21c5afac0f8a508de5ed20f36e00646f634bb1d
wangyuehong <wangyuehong@gmail.com>
2025-08-31 19:14:52 +0900

    fix(term): adjust keybinding for vterm escape handling
    - Updated `vterm-mode-map` to use "ESC ESC" for sending escape
    - Removed redundant keybinding for `vterm-copy-mode-map`

--- lisp/init-term.el
+++ lisp/init-term.el
@@ -14,9 +14,7 @@ and switch to `evil-emacs-state` otherwise."
       (evil-emacs-state)))
   :hook (vterm-copy-mode . my/vterm-copy-mode-evil-setup)
   :bind (:map vterm-mode-map
-          ("ESC" . vterm-send-escape)
-          :map vterm-copy-mode-map
-          ("ESC" . vterm-copy-mode-done))
+         ("ESC ESC" . vterm-send-escape))
   :custom
   (vterm-max-scrollback 100000))
 
