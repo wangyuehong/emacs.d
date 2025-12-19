00286099afe0dad0d6b2200a9d73fc33216abd87
wangyuehong <wangyuehong@gmail.com>
2025-12-19 23:27:59 +0900

    chore(keybindings): remove gptel-quick from AI menu
    - Remove unused "aq" keybinding for gptel-quick command

--- lisp/init-general.el
+++ lisp/init-general.el
@@ -71,7 +71,6 @@
     "a"  '(:wk "ai")
     "aa" '(gptel         :wk "gptel")
     "am" '(gptel-menu    :wk "gptel-menu")
-    "aq" '(gptel-quick   :wk "gptel-quick")
     "ar" '(gptel-rewrite :wk "gptel-rewrite")
     "as" '(gptel-send    :wk "gptel-send")
     "at" '(gptel-tools   :wk "gptel-tools")
