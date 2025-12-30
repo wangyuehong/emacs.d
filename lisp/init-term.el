bfec81d3d9b464192cef1d17579a2171e3924c94
wangyuehong <wangyuehong@gmail.com>
2025-12-30 17:08:19 +0900

    feat(term): add dynamic terminal title based on buffer name
    - Add function to set terminal title showing current buffer
    - Display filename for file buffers or buffer name otherwise
    - Enable hook only in terminal mode (non-graphic sessions)

--- lisp/init-term.el
+++ lisp/init-term.el
@@ -9,5 +9,16 @@
 
 (use-package emamux)
 
+(defun my/set-terminal-title ()
+  "Set terminal title to current buffer name."
+  (send-string-to-terminal
+   (format "\033]2;EMACS: %s\033\\"
+           (if buffer-file-name
+               (file-name-nondirectory buffer-file-name)
+             (buffer-name)))))
+
+(unless (display-graphic-p)
+  (add-hook 'buffer-list-update-hook #'my/set-terminal-title))
+
 (provide 'init-term)
 ;;; init-term.el ends here
